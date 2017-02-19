44d253a872874d8492032a294caff478e0844272
MichaelTd <tsouchlarakis@gmail.com>
2017-02-19 22:28:48 +0200

    Update start-up.sh

--- start-up.sh
+++ start-up.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+
 # X things
 xrdb ~/.Xresources 2> /dev/null
+
 # import functions
-#source /etc/wallpaper_rotate.source.sh
 source /etc/bash/bashrc.d/wallpaper-rotate.sh
 
 # Add some wallpaper variety for your desktop
 rotateBg &
+
 # Monitor your box
 #conky -DDDD -b -c ~/.conky.conf/.conkyrc.right.full >> ~/.conky.err/`date +%y%m%d`.conky.err.log 2>&1
 ~/bin/keepConkyAlive.sh &
 
-# quit screensaver if running
-xscreensaver-command -exit
-# Start screensaver in the background
-xscreensaver -nosplash &
+# XScreenSaver stuff
+
+xscreensaver-command -exit # quit screensaver if running
+
+xscreensaver -nosplash & # Start screensaver in the background
