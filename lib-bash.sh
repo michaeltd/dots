6c9caae455e18fc1b5abdc5213a8e1637ff4206c
MichaelTd <tsouchlarakis@gmail.com>
2017-06-19 05:25:52 +0300

    Mem

--- lib-bash.sh
+++ lib-bash.sh
@@ -1,5 +1,18 @@
 #!/bin/env /bin/bash
 
+function printMemUsage {
+
+	total=$(cat /proc/meminfo |head -1 |awk '{print $2}')
+	avail=$(cat /proc/meminfo |head -2 |tail -1 |awk '{print $2}')
+	used=$(expr ${total} - ${avail})
+	totalMB=$(expr ${total} / 1024)
+	availMB=$(expr ${avail} / 1024)
+	usedMB=$(expr ${used} / 1024)
+
+	printf "From a total of %d memory, you are using %d MB's, which leaves you with %d MB free.\n" $totalMB $usedMB $availMB
+
+}
+
 function rps {
   # Rock Paper Scissors mt 20170525
   declare -a op; declare -a oc; declare -A rs
