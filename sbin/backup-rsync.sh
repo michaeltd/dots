17fd0047f7e5886b2bf8bc9d26255250d9fc9b99
MichaelTd <tsouchlarakis@gmail.com>
2017-09-25 16:36:07 +0300

    home-dir

--- sbin/backup-rsync.sh
+++ sbin/backup-rsync.sh
@@ -2,14 +2,15 @@
 
 MAIL=paperjam@localhost
 
-nice=$(which nice)
 dtstp=$(date +%y%m%d.%H%M%S)
+nice=$(which nice)
 tarcm=$(which tar)
 rsncm=$(which rsync)
 eldir="/mnt/el/linux/gentoo/"
 dtdir="/mnt/DATA/linux/gentoo/"
-bulst="${HOME}/.backup.txt"
-excfl="${HOME}/.exclude.txt"
+homdr="/home/paperjam"
+bulst="${homdr}/.backup.txt"
+excfl="${homdr}/.exclude.txt"
 tarlg="/var/log/tar.${dtstp}.log"
 rslog="/var/log/rsync.${dtstp}.log"
 archv="/mnt/el/linux/gentoo/${HOSTNAME}.${USER}.${dtstp}.tar.gz"
@@ -19,7 +20,7 @@ if [[ -d "${eldir}" && -r "${bulst}" && -r "${excfl}"  && -x "${tarcm}" ]]; then
 
   "${nice}" -n 15 "${tarcm}" --exclude-from="${excfl}" -cvzf "${archv}" $(cat ${bulst}) >> "${tarlg}" 2>&1
 
-  #"${nice}" -n 15 "${tarcm}" --exclude="*/opt/*" --exclude="*/node_modules/*" --exclude="*/ImapMail/*" -cvzf "${fparv}" "${HOME}" >> "${tarlg}" 2>&1
+  #"${nice}" -n 15 "${tarcm}" --exclude="*/opt/*" --exclude="*/node_modules/*" --exclude="*/ImapMail/*" -cvzf "${fparv}" "${homdr}" >> "${tarlg}" 2>&1
 
 fi
 
