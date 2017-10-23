b38ab28c20dd56ce2c256a150c6dc3d03bde2df6
MichaelTd <tsouchlarakis@gmail.com>
2017-10-23 19:18:09 +0300

    readme

--- keepParamAlive.sh
+++ keepParamAlive.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env /bin/bash
 
 if [[ -z "${1}" || -z $(which "${1}") ]]; then # Test param
-  printf "Need an application as parameter.\n%s has not been found in \$PATH env var.\n" "${1}"
+  printf "Need an application as parameter.\n\"%s\" has not been found in \$PATH env var.\n" "${1}"
   exit 1
 else
   while [[ true ]]; do # Endless loop.
     pid=$(pgrep -x "${1}") # Get a pid.
     if [[ -z "${pid}" ]]; then # If there is none,
       "${1}" & # Start Param to background.
-    else # Else
-      sleep ${2-"60"} # Wait.
+    else
+      sleep ${2-"15"} # Wait.
     fi
   done
 fi
