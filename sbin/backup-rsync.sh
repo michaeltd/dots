0e04aa92626930dea9dac57f8761b31874fbe8b1
MichaelTd <tsouchlarakis@gmail.com>
2017-11-20 00:44:27 +0200

    admin

--- sbin/backup-rsync.sh
+++ sbin/backup-rsync.sh
@@ -7,26 +7,19 @@ nice=$(which nice)
 tarcm=$(which tar)
 rsncm=$(which rsync)
 machn="dell"
-eldir="/mnt/el/linux/gentoo/"
-dtdir="/mnt/data/linux/gentoo/"
+eldir="/mnt/el/linux/common"
+dtdir="/mnt/data/linux/common"
 homdr="/home/paperjam"
 bulst="${homdr}/.backup.txt"
 excfl="${homdr}/.exclude.txt"
 tarlg="/var/log/tar.${dtstp}.log"
 rslog="/var/log/rsync.${dtstp}.log"
-archv="/mnt/el/linux/gentoo/${machn}.${HOSTNAME}.${USER}.${dtstp}.tar.gz"
-fparv="/mnt/el/Documents/Videos/full.pj.${dtstp}.tar.gz"
+archv="${eldir}/${machn}.${HOSTNAME}.${USER}.${dtstp}.tar.gz"
 
 if [[ -d "${eldir}" && -r "${bulst}" && -r "${excfl}"  && -x "${tarcm}" ]]; then
-
   "${nice}" -n 15 "${tarcm}" --exclude-from="${excfl}" -cvzf "${archv}" $(cat ${bulst}) >> "${tarlg}" 2>&1
-
-  #"${nice}" -n 15 "${tarcm}" --exclude="*/opt/*" --exclude="*/node_modules/*" --exclude="*/ImapMail/*" -cvzf "${fparv}" "${homdr}" >> "${tarlg}" 2>&1
-
 fi
 
 if [[ -d "${eldir}" && -d "${dtdir}" && -x "${rsncm}" ]]; then
-
   "${nice}" -n 15 "${rsncm}" --verbose --recursive --times --delete --exclude="*/Videos/*" /mnt/el/* /mnt/data/ >> "${rslog}" 2>&1
-
 fi
