653ded25f6fce03fa94df917908427c7a9f2e543
MichaelTd <tsouchlarakis@gmail.com>
2017-11-25 05:07:45 +0200

    distro2package-manager

--- sbin/update-system.sh
+++ sbin/update-system.sh
@@ -1,36 +1,47 @@
 #!/usr/bin/env /bin/bash
 # Distro neutral update script tdm 171124
+# TODO --Make this package manager dependent rather than distro dependent--
 # From https://en.wikipedia.org/wiki/Package_manager
 # action      zypper        pacman        apt            dnf-yum            emerge
 # upd repo    zypper ref 	  pacman -Sy 	  apt update     yum check-update 	emerge --sync
 # upd sys     zypper up 	  pacman -Syu 	apt upgrade 	 yum update 	      emerge -uND --with-bdeps=y @world
 MAIL=paperjam@localhost
 
-set -aou
+set -aou #e
 
-function update-gentoo() {
-  emerge --sync
-  emerge -uDN --with-bdeps=y @world
-}
+declare zypper=$( which zypper )
+declare pacman=$( which pacman )
+declare apt-get=$( which apt-get )
+declare yum=$( which yum )
+declare emerge=$( which emerge )
 
-function update-opensuse() {
+function update-zypper() {
   # TODO add "assume-yes" switches here
   zypper update -ly # -l, --auto-agree-with-licenses / -y, --no-confirm
 }
 
-function update-debian() {
+function update-pacman() {
+  # TODO read the pacman manual
+  pacman -Sy
+  pacman -Syu
+}
+
+function update-apt-get() {
   # TODO add "assume-yes" switches here
   apt-get update
-  #apt-get -ys upgrade # Dry run
+  # apt-get -ys upgrade # Dry run
   apt-get -y upgrade # Wet run
 }
 
-function update-ubuntu() {
-  update-debian
+function update-yum() {
+  # TODO read the yum manual
+  yum check-update
+  yum update
 }
 
-function update-devuan() {
-  update-debian
+function update-emerge() {
+  emerge --sync
+  emerge -uDN --with-bdeps=y @world
 }
 
 function update-unknown() {
@@ -52,5 +63,29 @@ function get-distro() {
   return 1
 }
 
+function get-package-manager(){
+
+  if [[ ! -z "${zypper}" && -z "${pacman}" && -z "${apt-get}" && -z "${yum}" && -z "${emerge}" ]]; then
+    echo "zypper"
+    return 0
+  elif [[ -z "${zypper}" && ! -z "${pacman}" && -z "${apt-get}" && -z "${yum}" && -z "${emerge}" ]]; then
+    echo "pacman"
+    return 0
+  elif [[ -z "${zypper}" && -z "${pacman}" && ! -z "${apt-get}" && -z "${yum}" && -z "${emerge}" ]]; then
+    echo "apt-get"
+    return 0
+  elif [[ -z "${zypper}" && -z "${pacman}" && -z "${apt-get}" && ! -z "${yum}" && -z "${emerge}" ]]; then
+    echo "yum"
+    return 0
+  elif [[ -z "${zypper}" && -z "${pacman}" && -z "${apt-get}" && -z "${yum}" && ! -z "${emerge}" ]]; then
+    echo "emerge"
+    return 0
+  else
+    # Only reason to reach final clause would be custom setup, alien installed or otherwise system belonging to big boy who can do as well without our help. So gb and thanks for all the fish.
+    echo "unknown"
+    return 1
+  fi
+}
+
 # Make things happen.
-update-"$( get-distro )"
+update-"$( get-package-manager )"
