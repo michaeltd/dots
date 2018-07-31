dad2b69317c3c9d37b15cce23439caea3243f9d0
MichaelTd <tsouchlarakis@gmail.com>
2018-07-31 07:16:30 +0300

    plug-dots.sh

--- dot.files/bin/plug-dots.sh
+++ dot.files/bin/plug-dots.sh
@@ -3,7 +3,6 @@
 # ~/bin/plug-dots.sh
 # The means to migrate my .dots in new systems.
 
-# function runme() {
 # dtfls="${HOME}/git/lib-bash/dot.files"
 dtfls="$(cd ../$(dirname ${BASH_SOURCE[0]}) && pwd)" # One down from this one (bin/plug-dots.sh)
 tofldr="${HOME}"
@@ -17,17 +16,3 @@ for file in ${fls[@]}; do
   fi
   ln -s "${dtfls}/${file}" "${tofldr}/${file}"
 done
-# }
-
-# function dbg() {
-#   ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
-#   echo " ABSOLUTE_PATH is" $ABSOLUTE_PATH
-#   echo " BASH_SOURCE 0 is" ${BASH_SOURCE[0]}
-#   echo " basename BASH_SOURCE 0 is" $( basename "${BASH_SOURCE[0]}" )
-#   echo " dirname BASH_SOURCE 0 is" $( dirname "${BASH_SOURCE[0]}" )
-#   echo " cd dirname BASH_SOURCE 0 pwd is" $(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
-#   dtfls="$(cd ../$(dirname ${BASH_SOURCE[0]}) && pwd)" # One down from this one (bin/plug-dots.sh)
-#   echo " dtfls is" $dtfls
-# }
-
-# dbg
