bdf7f3f833731e29f6c770bac3c4546df5c04e53
MichaelTd <tsouchlarakis@gmail.com>
2018-08-01 22:18:52 +0300

    prompt-PS1

--- dot.files/bin/alarm.sh
+++ dot.files/bin/alarm.sh
@@ -4,4 +4,4 @@
 # at -M -f ~/bin/alarm.sh 08:00
 # 00 08 * * 1-5 /home/paperjam/bin/alarm.sh
 
-mpg123 -Z /mnt/data/Documents/Music/Stanley-Clarke/* /mnt/data/Documents/Music/Marcus-Miller/* /mnt/data/Documents/Music/Jaco-Pastorius/* /mnt/data/Documents/Music/Esperanza-Spalding/*
+mpg123 -Z -o pulse /mnt/data/Documents/Music/Stanley-Clarke/* /mnt/data/Documents/Music/Marcus-Miller/* /mnt/data/Documents/Music/Jaco-Pastorius/* /mnt/data/Documents/Music/Esperanza-Spalding/*
