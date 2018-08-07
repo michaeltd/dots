22d02440b3c31824269e443536bea59af8ad0f65
MichaelTd <tsouchlarakis@gmail.com>
2018-08-07 18:39:10 +0300

    alarm

--- dot.files/bin/alarm.sh
+++ dot.files/bin/alarm.sh
@@ -5,3 +5,6 @@
 # 00 08 * * 1-5 /home/paperjam/bin/alarm.sh
 
 mpg123 -Z -o pulse /mnt/data/Documents/Music/Stanley-Clarke/* /mnt/data/Documents/Music/Marcus-Miller/* /mnt/data/Documents/Music/Jaco-Pastorius/* /mnt/data/Documents/Music/Esperanza-Spalding/*
+
+#mocp -S /mnt/data/Documents/Music/Stanley-Clarke/* /mnt/data/Documents/Music/Marcus-Miller/* /mnt/data/Documents/Music/Jaco-Pastorius/* /mnt/data/Documents/Music/Esperanza-Spalding/*
+
