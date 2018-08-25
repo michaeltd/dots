24da93fb55b7f4a010e5a2fe9dc5b72df52eb1ae
MichaelTd <tsouchlarakis@gmail.com>
2018-08-25 04:27:31 +0300

    var

--- dot.files/bin/plug-dots.sh
+++ dot.files/bin/plug-dots.sh
@@ -3,11 +3,27 @@
 # ~/bin/plug-dots.sh
 # The means to migrate my .dots in new systems.
 
+function doExeptions {
+    # awesome
+    as=""
+    at=""
+    # openbox
+    os=""
+    ot=""
+    # tint2
+    ts=""
+    tt=""
+
+    return
+}
+
 # dtfls="${HOME}/git/lib-bash/dot.files"
 dtfls="$(cd ../$(dirname ${BASH_SOURCE[0]}) && pwd)" # One down from this one (bin/plug-dots.sh)
+
 tofldr="${HOME}"
 fx=".${RANDOM}"
 ls=$(which ls) # alias workaround
+
 declare -a fls=( $(${ls} -A ${dtfls}) )
 
 for file in ${fls[@]}; do
@@ -16,3 +32,5 @@ for file in ${fls[@]}; do
   fi
   ln -s "${dtfls}/${file}" "${tofldr}/${file}"
 done
+
+doExeptions
