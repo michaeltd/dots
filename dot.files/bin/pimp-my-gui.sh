3222e32764f44a365d09aa7884231ff09bf7ae62
MichaelTd <tsouchlarakis@gmail.com>
2018-09-27 19:09:15 +0300

    var

--- dot.files/bin/pimp-my-gui.sh
+++ dot.files/bin/pimp-my-gui.sh
@@ -20,31 +20,31 @@ function custom_run {
 #source /etc/os-release
 
 # XScreenSaver
-custom_run 19 xscreensaver -no-splash
+custom_run 9 xscreensaver -no-splash
 
 # A calendar app
-custom_run 19 orage
+custom_run 0 orage
 
 # Networking # Python gui
-nice -n 19 wicd-gtk -t &
+nice -n 0 wicd-gtk -t &
 
 # Start a terminal
 # Monitor your box
 source /etc/os-release
 if [[ "${ID}" == "devuan" ]]; then
-  custom_run 19 xfce4-terminal --disable-server
-  custom_run 19 conky >> /dev/null 2>&1
+  custom_run 0 xfce4-terminal --disable-server
+  custom_run 0 conky >> /dev/null 2>&1
 else
   # conky -c "${HOME}/git/dots/conky.configs/conky_configs/min_clock/conkyrc" >> /dev/null 2>&1 &
   # conky -c "${HOME}/git/dots/conky.configs/conky-horizontal-minimalist/conkyrc" >> /dev/null 2>&1 &
   # custom_run 19 conky -c "${HOME}/git/dots/conky.configs/old/qlock"
-  custom_run 19 terminology &
+  custom_run 0 terminology &
   #custom_run 19 conky -c "${HOME}/git/dots/conky.configs/conky_configs/min_clock/conkyrc" >> /dev/null 2>&1
-  custom_run 19 conky >> /dev/null 2>&1
+  custom_run 0 conky >> /dev/null 2>&1
 fi
 
 # Start a Menu
-nice -n 9 ${HOME}/bin/TkRootMenu &
+nice -n 0 ${HOME}/bin/TkRootMenu &
 
 # Add some wallpaper variety for your desktop
 ${HOME}/bin/wallpaper-rotate.sh &
