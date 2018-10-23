311265d9eb1966800ecd5f28284af3aa1733db45
MichaelTd <tsouchlarakis@gmail.com>
2018-10-23 04:55:24 +0300

    var

--- dot.files/bin/screenshot.sh
+++ dot.files/bin/screenshot.sh
@@ -5,4 +5,4 @@
 
 FN="${HOME}/ss-$(date +%s).png"
 
-import -delay 2 -window root "${FN}" || scrot --delay 2 "${FN}" # && ristretto "${FN}" || viewnior "${FN}"
+import -delay 2 -window root "${FN}" || scrot --delay 2 "${FN}" && ristretto "${FN}" || viewnior "${FN}"
