ba2b31d1c9f1f3ad543f5361f868141e6d3d442f
MichaelTd <tsouchlarakis@gmail.com>
2018-11-03 11:47:16 +0200

    colored-manpages

--- dot.files/.bashrc.d/variables.sh
+++ dot.files/.bashrc.d/variables.sh
@@ -27,6 +27,16 @@ else
   export BROWSER=$(which w3m 2> /dev/null||which links 2> /dev/null||which lynx 2> /dev/null)
 fi
 
+# Colorfull manpages (works with less as a pager)
+# https://www.tecmint.com/view-colored-man-pages-in-linux/
+export LESS_TERMCAP_mb=$'\e[1;32m'
+export LESS_TERMCAP_md=$'\e[1;32m'
+export LESS_TERMCAP_me=$'\e[0m'
+export LESS_TERMCAP_se=$'\e[0m'
+export LESS_TERMCAP_so=$'\e[01;33m'
+export LESS_TERMCAP_ue=$'\e[0m'
+export LESS_TERMCAP_us=$'\e[1;4;31m'
+
 # most > less > more in order of preference
 export PAGER=$(which most 2> /dev/null||which less 2> /dev/null||which more 2> /dev/null)
 
