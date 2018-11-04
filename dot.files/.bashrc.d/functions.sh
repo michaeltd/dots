a7eefc5e5f7d36c3c718062dff3fd43a53cd308f
MichaelTd <tsouchlarakis@gmail.com>
2018-11-04 07:23:16 +0200

    cleanup-bkps-prettified

--- dot.files/.bashrc.d/functions.sh
+++ dot.files/.bashrc.d/functions.sh
@@ -242,7 +242,7 @@ function wttr_in {
 function rps {
   # Rock Paper Scissors mt 20170525
 
-  declare -a op=("Rock" "Paper" "Scissors") oc=("WIN" "Defeat" "Draw")
+  declare -a op=("${red}Rock${reset}" "${green}Paper${reset}" "${blue}Scissors${reset}") oc=("${green}WIN${reset}" "${red}Defeat${reset}" "${blue}Draw${reset}")
 
   declare -A rs[0,0]=${oc[2]} rs[0,1]=${oc[1]} rs[0,2]=${oc[0]} \
              rs[1,0]=${oc[0]} rs[1,1]=${oc[2]} rs[1,2]=${oc[1]} \
@@ -250,23 +250,23 @@ function rps {
 
   declare cs=0 us=0 ns=0 rd=0
 
-  printf "Hello! Welcome to %s %s %s Game!\n" ${op[0]} ${op[1]} ${op[2]}
+  printf "${bold}Hello!${reset} Welcome to %s %s %s Game!\n" "${op[0]}" "${op[1]}" "${op[2]}"
 
   while [ true ]; do
 
     read -e -p "${op[0]}:1, ${op[1]}:2, ${op[2]}:3, Quit:0. What's your pick?: " ui
 
-    case ${ui} in
+    case "${ui}" in
       0)
-        if [ $us -gt $cs ] ; then
+        if (( us > cs )); then
           bbmsg="${oc[0]}"
-        elif [ $us -lt $cs ] ; then
+        elif (( us < cs )); then
           bbmsg="got ${oc[1]}ed by"
-        elif [ $us -eq $cs ] ; then
+        elif (( us == cs )); then
           bbmsg="${oc[2]}ed with"
         fi
 
-        printf "After %d rounds, you %s the CPU with %d:%d points and %d ties.\n" $rd ${bbmsg} $us $cs $ns
+        printf "After %d rounds, you %s the CPU with %d:%d points and %d ties.\n" "${rd}" "${bbmsg}" "${us}" "${cs}" "${ns}"
 
         return 0 ;;
 
@@ -276,18 +276,18 @@ function rps {
 
         ci=$(shuf -i 0-2 -n 1)
 
-        printf "Round : %d is a %s. You selected %s, while the CPU rolled %s\n" $rd ${rs[${ui},${ci}]}  ${op[${ui}]} ${op[${ci}]}
+        printf "Round : %d is a %s. You selected %s, while the CPU rolled %s\n" "${rd}" "${rs[${ui},${ci}]}"  "${op[${ui}]}" "${op[${ci}]}"
 
-        case ${rs[${ui},${ci}]} in
-          ${oc[0]}) let "us++";;
-          ${oc[1]}) let "cs++";;
-          ${oc[2]}) let "ns++";;
+        case "${rs[${ui},${ci}]}" in
+          "${oc[0]}") let "us++";;
+          "${oc[1]}") let "cs++";;
+          "${oc[2]}") let "ns++";;
         esac
 
-        printf "Player : %d, CPU : %d, Ties : %d\n" $us $cs $ns ;;
+        printf "Player : %d, CPU : %d, Ties : %d\n" "${us}" "${cs}" "${ns}";;
 
       *)
-        printf "Choose again from 0 to 3\n" ;;
+        printf "${red}Error${reset}: You chose ${red}%s${reset}. Choose again from 0 to 3\n" "${ui}" ;;
 
     esac
 
--- dot.files/sbin/cleanup-bkps.sh
+++ dot.files/sbin/cleanup-bkps.sh
@@ -7,18 +7,18 @@ source /home/paperjam/.bashrc.d/functions.sh
 
 cd "/mnt/el/Documents/bkps/linux"
 
-declare -a files=($(ls -t *.tar.gz))
+declare -a files=( $(ls -t *.tar.gz) )
 
 printf "# clean up old backups # -------------------------------------------------------\n"
 
 for (( x=0; x<"${#files[@]}"; x++ )); do
   if (( "x" > "1" )); then
-    fn="${files[$x]}"
-    do="$(digits_only ${fn})"
+    fn="${red}${files[$x]}${reset}"
+    do="$(digits_only ${files[$x]})"
     etdt="$(epochtodatetime ${do})"
-    printf "marked for removal -> %s\n" "${fn}"
-    printf "%s was created at %s\n" "${fn}" "${etdt}"
+    printf "${bold}${blue}marked for removal${reset} -> %s\n" "${fn}"
+    printf "%s was created at %s\n" "${fn}" "${underline}${green}${etdt}${reset}${end_underline}"
     # printf "\${#files[@]} is %s, \$x is : %s and \${files[\$x]} is \'%s\'\n" "${#files[@]}" "${x}" "${files[$x]}"
-    printf "rm -v ${fn}\n"
+    printf "${bold}rm -v ${fn}${reset}\n"
   fi
 done
