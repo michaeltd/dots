d2dec4fb042831798937ce918c858247aa7149ae
MichaelTd <tsouchlarakis@gmail.com>
2018-12-20 04:36:03 +0200

    comments

--- dot.files/.bashrc.d/functions.sh
+++ dot.files/.bashrc.d/functions.sh
@@ -206,6 +206,11 @@ function epochtodatetime {
 
 function lastdateofmonth {
 # https://en.wikipedia.org/wiki/Leap_year
+# if (year is not divisible by 4) then (it is a common year)
+# else if (year is not divisible by 100) then (it is a leap year)
+# else if (year is not divisible by 400) then (it is a common year)
+# else (it is a leap year)
+# https://stackoverflow.com/questions/3220163/how-to-find-leap-year-programatically-in-c/11595914#11595914
 # https://www.timeanddate.com/date/leapyear.html
 # https://medium.freecodecamp.org/test-driven-development-what-it-is-and-what-it-is-not-41fa6bca02a2
 # Feature: Every year that is exactly divisible by four is a leap year, except for years that are exactly divisible by 100, but these centurial years are leap years if they are exactly divisible by 400.
@@ -226,14 +231,10 @@ function lastdateofmonth {
   case "${m}" in
     "01"|"03"|"05"|"07"|"08"|"10"|"12") echo 31 ;;
     "02")
-      if (( y % 4 != 0 )); then # year is not divisible by 4 (it is a common year)
-        echo 28
-      elif (( y % 100 != 0 )); then # year is not divisible by 100 (it is a leap year)
-        echo 29
-      elif (( y % 400 != 0 )); then # year is not divisible by 400 (it is a common year)
-        echo 28
-      else # (it is a leap year)
-        echo 29
+      if   (( y % 4   != 0 )); then echo 28
+      elif (( y % 100 != 0 )); then echo 29
+      elif (( y % 400 != 0 )); then echo 28
+      else echo 29
       fi ;;
     "04"|"06"|"09"|"11") echo 30 ;;
   esac
