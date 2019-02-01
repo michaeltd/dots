d18d4f293b94ac0b69aa00501f425e0df9f6fddb
MichaelTd <tsouchlarakis@gmail.com>
2019-02-01 15:45:52 +0200

    upgrade-distro.sh

--- dot.files/sbin/upgrade-distro.sh
+++ dot.files/sbin/upgrade-distro.sh
@@ -7,7 +7,12 @@
 # For this to work package manager arrays must be in following format...
 # | #1 package manager executable | #2 repo update switch | #3 distro upgrade switch(es)| #4 ...
 # PS: By ignoring dpkg and rpm we are avoiding issues with systems where alien has been installed.
-declare -a zypper=( "zypper" "refresh" "update" "--no-confirm" "--auto-agree-with-licenses" ) pacman=( "pacman" "-Sy" "-Syu" ) apt_get=( "apt-get" "update" "--assume-yes" "--simulate" "dist-upgrade" ) yum=( "yum" "check-update" "update" ) emerge=( "emerge" "--sync" "--nospinner" "--pretend" "--update" "--deep" "--newuse" "--with-bdeps=y" "@world" ) pms=( zypper[@] pacman[@] apt_get[@] yum[@] emerge[@] )
+declare -a zypper=( "zypper" "refresh" "update" "--no-confirm" "--auto-agree-with-licenses" )
+declare -a pacman=( "pacman" "-Sy" "-Syu" )
+declare -a apt_get=( "apt-get" "update" "--assume-yes" "--simulate" "dist-upgrade" )
+declare -a yum=( "yum" "check-update" "update" )
+declare -a emerge=( "emerge" "--sync" "--nospinner" "--pretend" "--update" "--deep" "--newuse" "--with-bdeps=y" "@world" )
+declare -a pms=( zypper[@] pacman[@] apt_get[@] yum[@] emerge[@] )
 declare notfound="254"
 declare pmidx="${notfound}"
 
@@ -19,7 +24,7 @@ for x in "${!pms[@]}"; do
   fi
 done
 
-if (( "${pmidx}" == "${notfound}" && "${EUID}" != 0 )); then
+if (( "${pmidx}" == "${notfound}", "${EUID}" != 0 )); then
   printf "${red}Error:${reset} ${bold}Package manager not found, or non root privilages.${reset}\n For this to work you need ${bold}root${reset} account privilages and a \n ${underline}${green}%s${reset}${end_underline}, ${underline}${green}%s${reset}${end_underline}, ${underline}${green}%s${reset}${end_underline}, ${underline}${green}%s${reset}${end_underline} or ${underline}${green}%s${reset}${end_underline} based distro.\n Quithing.\n" "${!pms[0]:0:1}" "${!pms[1]:0:1}" "${!pms[2]:0:1}" "${!pms[3]:0:1}" "${!pms[4]:0:1}"
   exit 1
 else
