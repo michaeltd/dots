16557a16551a9a8e4984c93bd43d82126fda81d1
MichaelTd <tsouchlarakis@gmail.com>
2019-03-19 08:44:52 +0200

    maint

--- dot.files/sbin/daily/04-cleanup-bkps.sh
+++ dot.files/sbin/daily/04-cleanup-bkps.sh
@@ -18,8 +18,7 @@ for (( x = 0; x < ${#FLISTS[@]}; x++ )); do
       pfn="${red}${fn}${reset}"
       dp="${fn:0:10}"
       etdt="$(epochtodatetime ${dp})"
-      printf "${bold}${blue}marked for removal${reset} -> %s\n" "${pfn}"
-      printf "%s was created at %s\n" "${pfn}" "${underline}${green}${etdt}${reset}${end_underline}"
+      printf "${bold}${blue}will remove:${reset} %s, created: %s.\n" "${pfn}" "${underline}${green}${etdt}${reset}${end_underline}"
       printf "${bold}rm -v %s${reset}\n" "${pfn}"
       rm -v "${fn}"
     fi
