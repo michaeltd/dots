00912f1c19344a42584864a0fd8fb01b5716de9b
michaeltd <tsouchlarakis@gmail.com>
2019-03-26 17:25:52 +0200

    cleanupbkps

--- dot.files/sbin/cleanup-bkps.sh
+++ dot.files/sbin/cleanup-bkps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# de-clutter backups
+# ~/sbin/cleanup-bkps.sh - de-clutter backups
 
 # Load explicitly for non interactive shells
 source /home/paperjam/.bashrc.d/.stdl/time.sh
@@ -9,31 +9,34 @@ source /home/paperjam/.bashrc.d/.stdl/math.sh
 
 printf "= $(basename ${BASH_SOURCE[0]}) =\n"
 
-BKPD="/mnt/el/Documents/BKP/LINUX"
+BKPD="/mnt/el/Documents/BKP/LINUX" BKPK="-1"
 
-BKPK="-1"
-
-if [[ -d "${BKPD}" ]]; then
+if [[ -d "${BKPD}" ]]; then # Mount point check
     FILES=( $(ls -t ${BKPD}/*tar.gz* 2> /dev/null) )
     if [[ -n ${FILES[0]} ]]; then # Check empty list
         for (( x = 0; x < ${#FILES[@]}; x++ )); do # File loop
-            FN=$(basename "${FILES[$x]}"); FNS+=( "${FN}" )
+            FN=$(basename "${FILES[$x]}")
+            FNS+=( "${FN}" )
             for PART in $(split "${FN}" .); do # Name loop
-                if [[ "${PART}" =~ ^[0-9]+$ ]]; then
-                    if [[ "${PART}" =~ ^[0-9]{10}$ ]]; then
+                if [[ "${PART}" =~ ^[0-9]+$ ]]; then # Numeric part check
+                    if [[ "${PART}" =~ ^[0-9]{10}$ ]]; then # epoch check (10d)
                         EPS+=( "${PART}" )
-                    elif [[ "${PART}" =~ ^[0-9]{6}$ ]]; then
+                    elif [[ "${PART}" =~ ^[0-9]{8}$ ]]; then # 4 digits year date check
+                        DPS+=( "${PART}" )
+                    elif [[ "${PART}" =~ ^[0-9]{6}$ ]]; then # 2 digits year date check
                         DPS+=( "${PART}" )
                     fi
                 fi
             done
         done
 
-        MAXDT=$(max ${DPS[@]}) MINDT=$(min ${DPS[@]}); MAXDD=$(daydiff ${MAXDT} ${MINDT})
+        MAXDT=$(max ${DPS[@]}) MINDT=$(min ${DPS[@]})
+        MAXDD=$(daydiff ${MAXDT} ${MINDT})
 
-        if (( MAXDD > BKPK )); then
-            for (( y = 0; y < ${#FNS[@]}; y++ )); do
-                FN=${FNS[$y]} DP=${DPS[$y]} EP=${EPS[$y]}; DD=$(daydiff ${MAXDT} ${DP})
+        if (( MAXDD > BKPK )); then # BacK uP Keep threshold check
+            for (( y = 0; y < ${#FNS[@]}; y++ )); do # File NameS loop
+                FN=${FNS[$y]} DP=${DPS[$y]} EP=${EPS[$y]}
+                DD=$(daydiff ${MAXDT} ${DP})
 
                 if (( DD > BKPK )); then
                     DT="$(date --date=${DP} +%Y/%m/%d)"
