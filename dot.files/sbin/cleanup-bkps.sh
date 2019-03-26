1ae82a2b4f82e4da35debb28767915d8047e2a1d
michaeltd <tsouchlarakis@gmail.com>
2019-03-26 16:29:28 +0200

    house

--- dot.files/sbin/cleanup-bkps.sh
+++ dot.files/sbin/cleanup-bkps.sh
@@ -14,32 +14,60 @@ BKPD="/mnt/el/Documents/BKP/LINUX"
 BKPK="-1"
 
 if [[ -d "${BKPD}" ]]; then
+
     FILES=( $(ls -t ${BKPD}/*tar.gz* 2> /dev/null) )
-    for (( y = 0; y < ${#FILES[@]}; y++ )); do # File loop
-        FN=$(basename "${FILES[$y]}"); FNS+=( "${FN}" )
-        for PART in $(split "${FN}" .); do # Name loop
-            if [[ "${PART}" =~ ^[0-9]+$ ]]; then
-                if [[ "${PART}" =~ ^[0-9]{10}$ ]]; then
-                    EPS+=( "${PART}" )
-                elif [[ "${PART}" =~ ^[0-9]{6}$ ]]; then
-                    DPS+=( "${PART}" )
+
+    if [[ -n ${FILES[0]} ]]; then # Check empty list
+
+        for (( y = 0; y < ${#FILES[@]}; y++ )); do # File loop
+
+            FN=$(basename "${FILES[$y]}"); FNS+=( "${FN}" )
+
+            for PART in $(split "${FN}" .); do # Name loop
+
+                if [[ "${PART}" =~ ^[0-9]+$ ]]; then
+
+                    if [[ "${PART}" =~ ^[0-9]{10}$ ]]; then
+
+                        EPS+=( "${PART}" )
+
+                    elif [[ "${PART}" =~ ^[0-9]{6}$ ]]; then
+
+                        DPS+=( "${PART}" )
+
+                    fi
+
                 fi
-            fi
-        done
-    done
 
-    MAXDT=$(max ${DPS[@]}) MINDT=$(min ${DPS[@]}); MAXDD=$(daydiff ${MAXDT} ${MINDT})
+            done
 
-    if (( MAXDD > BKPK )); then
-        for (( i = 0; i < ${#FNS[@]}; i++ )); do
-            FN=${FNS[$i]} DP=${DPS[$i]} EP=${EPS[$i]}; DD=$(daydiff ${MAXDT} ${DP})
-            if (( DD > BKPK )); then
-                DT="$(date --date=${DP} +%Y/%m/%d)"
-                printf "${bold}${blue}will remove:${reset} %s, created: %s.\n" "${red}${FN}${reset}" "${underline}${green}${DT}${reset}${end_underline}"
-                printf "${bold}rm -v %s${reset}: \n" "${red}${FN}${reset}"
-                #rm -v "${BKPD}/${FN}"
-            fi
         done
+
+
+        MAXDT=$(max ${DPS[@]}) MINDT=$(min ${DPS[@]}); MAXDD=$(daydiff ${MAXDT} ${MINDT})
+
+
+        if (( MAXDD > BKPK )); then
+
+            for (( i = 0; i < ${#FNS[@]}; i++ )); do
+
+                FN=${FNS[$i]} DP=${DPS[$i]} EP=${EPS[$i]}; DD=$(daydiff ${MAXDT} ${DP})
+
+                if (( DD > BKPK )); then
+
+                    DT="$(date --date=${DP} +%Y/%m/%d)"
+
+                    printf "${bold}${blue}will remove:${reset} %s, created: %s.\n" "${red}${FN}${reset}" "${underline}${green}${DT}${reset}${end_underline}"
+
+                    printf "${bold}rm -v %s${reset}: \n" "${red}${FN}${reset}"
+
+                    #rm -v "${BKPD}/${FN}"
+
+                fi
+
+            done
+
+        fi
     fi
 else
     printf "${BKPD} not found\n" >&2
