c3256763fe5b3f9aa32bc5baea2c1a96d94811a9
michaeltd <tsouchlarakis@gmail.com>
2019-04-14 18:58:54 +0300

    Todo!!!

--- dot.files/.bashrc.d/misc.sh
+++ dot.files/.bashrc.d/misc.sh
@@ -1,12 +1,22 @@
-# ~/.bashrc.d/misc
+# ~/.bashrc.d/misc.sh
 #
 # general bash options
-
-# Add to history instead of overriding it
-shopt -s histappend
 
 # Window size sanity check
 shopt -s checkwinsize
 
 # pandoc bash completion
 # eval "$(pandoc --bash-completion)"
+
+# https://twitter.com/gumnos/status/1117146713289121797
+# And a couple bash options to control how history is stored:
+HISTCONTROL=ignorespace:erasedups
+HISTIGNORE=ll:ls:cd:pwd:
+HISTSIZE=10000
+HISTFILESIZE=20000
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# and keep synced:
+export PROMPT_COMMAND='history -a'
