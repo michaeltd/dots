56f9cc51b582d90c01601a11d99cbbb54e646be7
michaeltd <tsouchlarakis@gmail.com>
2019-04-16 05:34:38 +0300

    another big bag of changes

--- dot.files/sbin/update-mirror.sh
+++ dot.files/sbin/update-mirror.sh
@@ -4,11 +4,11 @@
 # Update my data
 
 # Full path executables
-NICEC=$(which nice) RSNCM=$(which rsync) ELMNT="/mnt/el/PStart.xml" DTMNT="/mnt/data/PStart.xml"
+NICEC=$(which nice) RSNCM=$(which rsync) ELMNT="/mnt/el/Documents" DTMNT="/mnt/data/Documents"
 
 printf "= $(basename ${BASH_SOURCE[0]}) =\n"
 
-if [[ -f "${ELMNT}" && -f "${DTMNT}" ]]; then
+if [[ -d "${ELMNT}" && -d "${DTMNT}" ]]; then
     time "${NICEC}" -n 19 "${RSNCM}" --verbose --recursive --times --delete --exclude="*/MSOFT/*" /mnt/el/* /mnt/data/
 else
     printf "${ELMNT} or ${DTMNT} not found\n" >&2
