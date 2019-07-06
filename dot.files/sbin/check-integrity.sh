91e2aa1e3d1efa26db33ff9cd79dd33c6c94c7a2
michaeltd <tsouchlarakis@gmail.com>
2019-07-06 11:21:23 +0300

    bad things happen when you forget about that one little change you made ages ago

--- dot.files/sbin/check-integrity.sh
+++ dot.files/sbin/check-integrity.sh
@@ -3,9 +3,6 @@
 # ~/sbin/check-integrity.sh
 # man qcheck : qcheck - verify integrity of installed packages
 
-# qclog="/var/log/qcheck.${HOSTNAME}.${USER}.$(date +%y%m%d.%H%M%S).log"
-
 printf "= $(basename ${BASH_SOURCE[0]}) =\n"
 
-#(( EUID == 0 )) && time qcheck --quiet --badonly| xargs qcheck
 (( EUID == 0 )) && time qcheck
