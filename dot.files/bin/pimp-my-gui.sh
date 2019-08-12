0ae46ba64e75486a1bae4ffdd153dba391a51f3d
michaeltd <tsouchlarakis@gmail.com>
2019-08-12 16:03:03 +0300

    grmbl

--- dot.files/bin/pimp-my-gui.sh
+++ dot.files/bin/pimp-my-gui.sh
@@ -24,7 +24,7 @@ rcm 9 ~/bin/wallpaper-rotate.sh
 # Per distro setup.
 source /etc/os-release
 if [ "${ID}" == "gentoo" ]; then
-  rcm 9 terminology
+  #rcm 9 terminology
   sleep 5 && rcm 9 conky -qdc ~/.conky/ConkyNeon/conkyrc
 elif [ "${ID}" == "devuan" ]; then
   rcm 9 xfce4-terminal --disable-server
