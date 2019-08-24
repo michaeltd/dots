d6f188fc5aea70ffd682ce3b2a8d29b34c777e78
michaeltd <tsouchlarakis@gmail.com>
2019-08-24 07:34:01 +0300

    Fixed the build.

--- dot.files/.bashrc.d/functions.sh
+++ dot.files/.bashrc.d/functions.sh
@@ -172,7 +172,7 @@ function services {
     printf "%s requires some parameters.\nUsage: %s start|stop|restart all|service/es...\n" "${FUNCNAME[0]}" "${FUNCNAME[0]}"
     return 1
   elif [[ ("${1}" == "start" || "${1}" == "stop" || "${1}" == "restart" || "${1}" == "status") && ("${2}" == "all") ]]; then
-    declare -a srvcs=( "postgresql-11" "mysql" "mongodb" "apache2" "tomcat" "vsftpd" "sshd" "rsyncd" "dictd" "netdata" "webmin" )
+    declare -a srvcs=( "postgresql-11" "mysql" "mongodb" "apache2" "tomcat" "vsftpd" "sshd" "rsyncd" "dictd" )
   else
     declare -a srvcs=( "${@}" )
     unset srvcs[0]
