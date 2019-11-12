e06bb7c7891d3a68e02f44ae5a2f96ffa2d94771
michaeltd <tsouchlarakis@gmail.com>
2019-11-12 12:07:21 +0200

    Derp. Fix missing constant post rename

--- dot.files/.bashrc.d/.stdl/speed.sh
+++ dot.files/.bashrc.d/.stdl/speed.sh
@@ -2,10 +2,10 @@
 #
 # Speed conversions
 
-function kmphtomph {
-    printf "%.2f\n" $(echo "scale=2;${1} * 0.6213712"|bc)
+kmphtomph() {
+  printf "%.2f\n" $(echo "scale=2;${1} * 0.6213712"|bc)
 }
 
-function mphtokmph {
-    printf "%.2f\n" $(echo "scale=2;${1} * 1.609344"|bc)
+mphtokmph() {
+  printf "%.2f\n" $(echo "scale=2;${1} * 1.609344"|bc)
 }
