b7d944483b1f354719fa745ecf253a2cf340ef3f
michaeltd <tsouchlarakis@gmail.com>
2019-11-13 07:01:26 +0200

    More ignore

---
+++ dot.files/bin/alarm.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# ~/bin/alarm.sh
+#
+# crontab
+# For details see man 4 crontabs
+# Example of job definition:
+# .---------------- minute (0 - 59)
+# |  .------------- hour (0 - 23)
+# |  |  .---------- day of month (1 - 31)
+# |  |  |  .------- month (1 - 12) OR jan,feb,mar,apr ...
+# |  |  |  |  .---- day of week (0 - 6) (Sunday=0 or 7) OR sun,mon,tue,wed,thu,fri,sat
+# |  |  |  |  |
+# *  *  *  *  * user-name  command to be executed
+# 00 08 * * 1-5 ~/bin/alarm.sh
+#
+# 00 07 * * 1-5 bash -lc "at -m -f ~/bin/alarm.sh now + 1 hour"
+#
+# at -m -f ~/bin/alarm.sh now
+
+printf "= $(basename ${0}) =\n"
+
+# mpg123 -Z /mnt/data/Documents/Music/Stanley-Clarke/* /mnt/data/Documents/Music/Marcus-Miller/* /mnt/data/Documents/Music/Jaco-Pastorius/* /mnt/data/Documents/Music/Esperanza-Spalding/* /mnt/data/Documents/Music/Mark-King/Level\ Best/*
+
+# mplayer -shuffle -msglevel all=3 /mnt/data/Documents/Music/Stanley-Clarke/* /mnt/data/Documents/Music/Marcus-Miller/* /mnt/data/Documents/Music/Jaco-Pastorius/* /mnt/data/Documents/Music/Esperanza-Spalding/* /mnt/data/Documents/Music/Mark-King/Level\ Best/*
+
+cvlc --random file:///mnt/data/Documents/Music/Stanley-Clarke/ file:///mnt/data/Documents/Music/Marcus-Miller/ file:///mnt/data/Documents/Music/Jaco-Pastorius/ file:///mnt/data/Documents/Music/Esperanza-Spalding/ file:///mnt/data/Documents/Music/Mark-King/Level\ Best/
---
+++ dot.files/bin/pimp_my_gui.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# ~/bin/pimp-my-gui.sh
+# Spice for the desktop
+
+# No double sourcing
+[[ ! $(command -v rcm) ]] && source ~/.bashrc.d/functions.sh
+
+# Run emacs
+rcm 0 emacs --daemon
+
+# Xfce4 themes
+rcm 9 xfsettingsd --no-daemon --replace
+
+# XScreenSaver
+rcm 9 xscreensaver # -no-splash
+
+# Add some wallpaper variety for your desktop
+rcm 9 ~/bin/wallpaper_rotate.sh
+
+# Per distro setup.
+source /etc/os-release
+if [ "${ID}" == "gentoo" ]; then
+  #rcm 9 terminology
+  #rcm 9 conky -qdc ~/.conky/ConkyNeon/conkyrc
+  sleep 30; rcm 9 conky -qdc ~/.conky/FreeClox/conkyrc
+elif [ "${ID}" == "devuan" ]; then
+  rcm 9 xfce4-terminal --disable-server
+  rcm 9 conky -qd
+else
+  rcm 9 xterm
+  rcm 9 conky -qd
+fi
