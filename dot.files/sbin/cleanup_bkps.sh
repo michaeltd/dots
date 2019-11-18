ddbecc336a8abe67eae9f9e3b8a496bd8ea4346c
michaeltd <tsouchlarakis@gmail.com>
2019-11-18 17:50:39 +0200

    add actual words

--- dot.files/sbin/cleanup_bkps.sh
+++ dot.files/sbin/cleanup_bkps.sh
@@ -16,41 +16,24 @@ BKPK="14"
 # BacKuPs Remove => BKPR (1 remove, 0 don't)
 BKPR="1"
 
-declare -a srcs=( /home/paperjam/.bashrc.d/.stl/time.sh /home/paperjam/.bashrc.d/.stl/string.sh /home/paperjam/.bashrc.d/.stl/math.sh )
+# Load explicitly for non interactive shells.
+# source /home/paperjam/.bashrc.d/.stl/time.sh # for datedd()
+# source /home/paperjam/.bashrc.d/.stl/string.sh # for split()
+# source /home/paperjam/.bashrc.d/.stl/math.sh # for max()
+declare -a srcs=( /home/paperjam/.bashrc.d/.stl/time.sh \
+                    /home/paperjam/.bashrc.d/.stl/string.sh \
+                    /home/paperjam/.bashrc.d/.stl/math.sh )
 
-printf " -- %s --\n" "$(basename ${BASH_SOURCE[0]})"
-
-if [[ -n "${1}" ]]
-then
-
-  while [[ -n "${1}" ]]
-  do
-
-    case "${1}" in
-
-      "--directory")
-
-        shift; BKPD="${1}"
-        ;;
-      "--simulate")
-
-        BKPR="0"
-        ;;
-      "--remove")
-
-        BKPR="1"
-        ;;
-      *)
-
-        printf "Usage: $(basename ${BASH_SOURCE[0]}) [--directory /backups/directory/] ([--simulate]|[--remove (default)])\n" >&2
-
-        exit 1
-        ;;
-    esac
-
-    shift
-  done
-fi
+while [[ -n "${1}" ]]
+do
+  case "${1}" in
+    "--directory") shift; BKPD="${1}";;
+    "--simulate") BKPR="0";;
+    "--keep") shift; BKPK="${1}";;
+    *) printf "Usage: $(basename ${BASH_SOURCE[0]}) [--directory /backups/directory/] [--simulate] [--keep 10 (int, days. Default: 14)]\n" >&2; exit 1;;
+  esac
+  shift
+done
 
 # No root access
 # (( EUID != 0 )) && printf "privileged access requirements not met.\n" >&2 && exit 1
@@ -58,35 +41,30 @@ fi
 # No backups directory
 [ ! -d "${BKPD}" ] && printf "${BKPD} is not a directory.\n" >&2 && exit 1
 
-# Load explicitly for non interactive shells.
-# source /home/paperjam/.bashrc.d/.stl/time.sh # for datedd()
-# source /home/paperjam/.bashrc.d/.stl/string.sh # for split()
-# source /home/paperjam/.bashrc.d/.stl/math.sh # for max()
+printf " -- %s --\n" "$(basename ${BASH_SOURCE[0]})"
 
 for src in "${srcs[@]}"
 do
   source "${src}"
 done
 
-
 FILES=( $($(which ls) -t1 ${BKPD}/*tar.gz* 2> /dev/null) )
 
 # File loop to gather stats
 for (( x = 0; x < ${#FILES[@]}; x++ ))
 do
-
-  BFN="$(basename ${FILES[$x]})"
-
+  BFN="$(basename ${FILES[${x}]})"
   # Name loop to extract dates
   for PART in $(split ${BFN} .)
   do
-
     # 6 digits field check (six digit dates eg: 190508)
     if [[ "${PART}" =~ ^[0-9]{6}$ ]]
     then
-
       FNS+=( "${BFN}" )
       DTS+=( "${PART}" )
+    elif [[ "${PART}" =~ ^[0-9]{10}$ ]]
+    then
+      TSS+=( "${PART}" )
     fi
   done
 done
@@ -94,16 +72,12 @@ done
 # File NameS loop to execute on stats
 for (( y = 0; y < ${#FNS[@]}; y++ ))
 do
-
-  if (( $(datedd $(max ${DTS[@]}) ${DTS[$y]}) >= BKPK ))
+  if (( $(datedd $(max ${DTS[@]}) ${DTS[${y}]}) >= BKPK ))
   then
-
     printf "${bold}${blue}will remove:${reset} %s, created: %s.\n" \
-           "${red}${FNS[$y]}${reset}" \
-           "${underline}${green}$(date -d ${DTS[$y]} +%Y/%m/%d_%H:%M:%S)${reset}${end_underline}"
-
-    printf "${bold}rm -v %s${reset}: " "${red}${FNS[$y]}${reset}"
-
-    (( BKPR == 0 )) && printf "\n" || rm -v "${BKPD}/${FNS[$y]}"
+           "${red}${FNS[${y}]}${reset}" \
+           "${underline}${green}$(date -d @${TSS[${y}]} +%Y/%m/%d_%H:%M:%S)${reset}${end_underline}"
+    printf "${bold}rm -v %s${reset}: " "${red}${FNS[${y}]}${reset}"
+    (( BKPR == 0 )) && printf "\n" || rm -v "${BKPD}/${FNS[${y}]}"
   fi
 done
