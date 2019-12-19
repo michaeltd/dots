77bb839b0bf63c1445241d8ebf13bc24119788da
michaeltd <tsouchlarakis@gmail.com>
2019-12-19 15:22:03 +0200

    Herping the fucking derp right here and now.

--- dot.files/.e16/Init/e16.sh
+++ dot.files/.e16/Init/e16.sh
@@ -1,15 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 #
 #shellcheck source=/dev/null
 
 # No double sourcing
-[ ! $(command -v rcm) ] && . ~/.bashrc.d/functions.sh
+[[ -z "$(command -v rcm)" ]] && source ~/.bashrc.d/functions.sh
 
-# rcm 9 wicd-gtk -t
+rcm 9 wicd-gtk -t
 
-# rcm 9 pasystray -a -m 100
+rcm 9 pasystray -a -m 100
+
+rcm 9 orage
 
 PMG="${HOME}/bin/pimp_my_gui.sh"
-if [ -x "${PMG}" ]; then # If spice ...
+if [[ -x "${PMG}" ]]; then # If spice ...
   "${PMG}" & # Spice things up
 fi
