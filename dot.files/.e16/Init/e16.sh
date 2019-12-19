97eea61c96f9d7e3c3440166e176f19ebb000a72
michaeltd <tsouchlarakis@gmail.com>
2019-12-19 15:31:11 +0200

    touched...

--- dot.files/.e16/Init/e16.sh
+++ dot.files/.e16/Init/e16.sh
@@ -5,11 +5,11 @@
 # No double sourcing
 [[ -z "$(command -v rcm)" ]] && source ~/.bashrc.d/functions.sh
 
-rcm 9 wicd-gtk -t
+# rcm 9 wicd-gtk -t
 
-rcm 9 pasystray -a -m 100
+# rcm 9 pasystray -a -m 100
 
-rcm 9 orage
+# rcm 9 orage
 
 PMG="${HOME}/bin/pimp_my_gui.sh"
 if [[ -x "${PMG}" ]]; then # If spice ...
