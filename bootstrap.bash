609c6876a9c42ad56dcc701790b653973408a22b
michaeltd <tsouchlarakis@gmail.com>
2019-12-26 10:23:23 +0200

    I know what I am doing. Trust me.

--- bootstrap.bash
+++ bootstrap.bash
@@ -6,14 +6,14 @@
 set -e
 
 if [[ "${1}" != "thoushallnotpass" ]]; then
-  #shellcheck disable=SC2154
-  echo "${red}Read this first:${reset} ${bold}https://github.com/michaeltd/dots/blob/master/readme.md#bootstrap.sh${reset}" >&2
-  exit 1
+    #shellcheck disable=SC2154
+    echo "${red}Read this first:${reset} ${bold}https://github.com/michaeltd/dots/blob/master/readme.md#bootstrap.sh${reset}" >&2
+    exit 1
 else
     shift
 fi
 
-declare -A __shells=( \
+declare -A __console=( \
 ['.bash_logout']='dot.files/.bash_logout' \
 ['.bash_profile']='dot.files/.bash_profile' \
 ['.bashrc']='dot.files/.bashrc' \
@@ -22,20 +22,21 @@ declare -A __shells=( \
 ['.tcshrc']='dot.files/.tcshrc' \
 ['bin']='dot.files/bin' \
 ['sbin']='dot.files/sbin' \
-['.bkp.exclude']='dot.files/.bkp.exclude' )
+['.bkp.exclude']='dot.files/.bkp.exclude' \
+['.vimrc']='dot.files/.vimrc' \
+['.gvimrc']='dot.files/.gvimrc' \
+['.muttrc']='dot.files/.muttrc' \
+['.public.pgp.key']='dot.files/.public.pgp.key' \
+['.tmux.conf']='dot.files/.tmux.conf' \
+['.config/mpd/mpd.conf']='dot.files/.config/mpd/mpd.conf' \
+['.config/ncmpcpp/config']='dot.files/.config/ncmpcpp/config')
 
 declare -A __xorg=( \
 ['.Xdefaults']='dot.files/.Xdefaults' \
 ['.Xresources']='dot.files/.Xresources' \
 ['.Xresources.d']='dot.files/.Xresources.d' \
-['.xinitrc']='.xinitrc' \
-['.xsession']='.xsession' )
-
-declare -A __utils=( \
-['.gitconfig']='dot.files/.gitconfig' \
-['.public.pgp.key']='dot.files/.public.pgp.key' )
-
-declare -A __wms=( \
+['.xinitrc']='dot.files/.xinitrc' \
+['.xsession']='dot.files/.xsession' \
 ['.config/awesome']='dot.files/.config/awesome' \
 ['.config/compiz']='dot.files/.config/compiz' \
 ['.config/openbox']='dot.files/.config/openbox' \
@@ -44,7 +45,15 @@ declare -A __wms=( \
 ['.fvwm-crystal']='dot.files/.fvwm-crystal' \
 ['GNUstep']='dot.files/GNUstep' \
 ['.mwmrc']='dot.files/.mwmrc' \
-['Mwm']='dot.files/Mwm' )
+['Mwm']='dot.files/Mwm' \
+['.config/polybar']='dot.files/.config/polybar' \
+['.config/tint2']='dot.files/.config/tint2' \
+['.config/compton.conf']='dot.files/.config/compton.conf' \
+['.config/gentoo/gentoorc']='dot.files/.config/gentoo/gentoorc' \
+['.config/mc/ini']='dot.files/.config/mc/ini' \
+['.config/mc/panels.ini']='dot.files/.config/mc/panels.ini' \
+['.config/doublecmd/doublecmd.xml']='dot.files/.config/doublecmd/doublecmd.xml' \
+['.config/doublecmd/shortcuts.scf']='dot.files/.config/doublecmd/shortcuts.scf' )
 
 declare -A __security=( \
 ['.gnupg']='dot.files/.gnupg' \
@@ -54,32 +63,58 @@ declare -A __security=( \
 ['.config/filezilla']='dot.files/.config/filezilla' \
 ['.config/hexchat']='dot.files/.config/hexchat' )
 
-declare -A __editors=( \
-['.vimrc']='dot.files/.vimrc' \
-['.gvimrc']='dot.files/.gvimrc' )
+__usage(){
+    echo "Usage: $(basename ${BASH_SOURCE[0]}) -(-a)ll|-(-c)onsole|-(-x)org|-(-s)ecurity|--help"
+}
 
-declare -A __email=( \
-['.muttrc']='dot.files/.muttrc' \
-['.public.pgp.key']='dot.files/.public.pgp.key' \
-)
+__check_console() {
+    for i in "${!__console[@]}"; do
+	if [[ ! -e "${__console[${i}]}" ]]; then
+	    echo "fatal: ${__console[${i}]} not found"
+	    exit 1
+	fi
+    done
+}
 
-declare -A __tmux=(['.tmux.conf']='dot.files/.tmux.conf')
+__check_xorg() {
+    for i in "${!__xorg[@]}"; do
+	if [[ ! -e "${__xorg[${i}]}" ]]; then
+	    echo "fatal: ${__xorg[${i}]} not found"
+	    exit 1
+	fi
+    done
+}
 
-declare -A __music=( \
-['.config/mpd/mpd.conf']='dot.files/.config/mpd/mpd.conf' \
-['.config/ncmpcpp/config']='dot.files/.config/ncmpcpp/config')
+__check_security() {
+    for i in "${!__security[@]}"; do
+	if [[ ! -e "${__security[${i}]}" ]]; then
+	    echo "fatal: ${__security[${i}]} not found"
+	    exit 1
+	fi
+    done
+}
 
-declare -A __fms=( \
-['.config/gentoo/gentoorc']='dot.files/.config/gentoo/gentoorc' \
-['.config/mc/ini']='dot.files/.config/mc/ini' \
-['.config/mc/panels.ini']='dot.files/.config/mc/panels.ini' \
-['.config/doublecmd/doublecmd.xml']='dot.files/.config/doublecmd/doublecmd.xml' \
-['.config/doublecmd/shortcuts.scf']='dot.files/.config/doublecmd/shortcuts.scf' )
+__check_all() {
+    __check_console
+    __check_xorg
+    __check_security
+}
 
-declare -A __desktop=( \
-['.config/polybar']='dot.files/.config/polybar' \
-['.config/tint2']='dot.files/.config/tint2' \
-['.config/compton.conf']='dot.files/.config/compton.conf' )
+__do_all() {
+    __check_all
+}
+
+__do_console() {
+    __check_console
+}
+
+__do_xorg() {
+    __check_xorg
+}
+
+__do_security() {
+    __check_security
+}
 
 # DTFLS="$(cd "$(dirname "${0}")/dot.files" && pwd -P)"
 # TOFLD="${HOME}"
@@ -94,27 +129,17 @@ declare -A __desktop=( \
 #   ln -sf "${DTFLS}/${FL}" "${TOFLD}/${FL}"
 # done
 
-__do_checks(){
-    for i in "${!__shells[@]}"; do
-	if [[ ! -e "${__shells[${i}]}" ]]; then
-	    echo "fatal: ${__shells[${i}]} not found"
-	    exit 1
-	fi
-    done
-}
-
-__usage(){
-    echo "Usage: $0 -e -m"
-}
-
 if [[ -n "${1}" ]]; then
     while [[ -n "${1}" ]]; do
     	  case ${1} in
-	       -e) shift;;
-	       -m) shift;;
-	       *) shift; __usage;;
+	       -a|--all) shift; __do_all;;
+	       -c|--console) shift; __do_console;;
+	       -x|--xorg) shift; __do_xorg;;
+	       -s|--security) shift; __do_security;;
+	       *) shift; __usage; exit 1;;
 	  esac
     done
+else
+    __usage
+    exit 1
 fi
-
-__do_checks
