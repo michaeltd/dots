b5731ea956088ddc46cc518669e16bbfc9ef9c1e
michaeltd <tsouchlarakis@gmail.com>
2019-12-26 06:56:15 +0200

    Refactored configuration.

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,3 +23,27 @@ for FL in $("${LS}" "-A" "${DTFLS}"); do
   fi
   ln -sf "${DTFLS}/${FL}" "${TOFLD}/${FL}"
 done
+
+# declare -A __shell=( [.bash_logout]="dot.files/.bash_logout" \
+# 				   [.bash_profile]="dot.files/.bash_profile" \
+# 				   [.bashrc]="dot.files/.bashrc" \
+# 				   [.bashrc.d]="dot.files/.bashrc.d" \
+# 				   [.profile]="dot.files/.profile" \
+# 				   [.tcshrc]="dot.files/.tcshrc" \
+# 				   [bin]="dot.files/bin" \
+# 				   [sbin]="dot.files/sbin" \
+# )
+
+# declare -A __xorg=( [.Xdefaults]="dot.files/.Xdefaults" \
+# 				[.Xresources]="dot.files/.Xresources" \
+# 				[.Xresources.d]="dot.files/.Xresources.d" \
+# 				[.xinitrc]=".xinitrc" \
+# 				[.xsession]=".xsession" \
+# )
+
+# declare -A __utils=( [.bkp.exclude]=".bkp.exclude" \
+# 				   [.crontab]=".crontab" \
+# 				   [.gitconfig]=".gitconfig" \
+# 				   [.public.pgp.key]=".public.pgp.key" \
+# )
+
