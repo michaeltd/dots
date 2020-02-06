1b9f5ecb5fac1aa18ccb585091ae73383eba65ec
michaeltd <tsouchlarakis@gmail.com>
2020-02-06 13:25:21 +0200

    Insert Commit Message Here

--- dot.files/.bashrc.d/aliases.bash
+++ dot.files/.bashrc.d/aliases.bash
@@ -48,7 +48,7 @@ elif type -P pacman &> /dev/null; then
   alias dupdate='sudo pacman -Sy' dupgrade='sudo pacman -Syu' dcleanup='sudo pacman -Rsn'
 elif type -P emerge &> /dev/null; then
   alias psearch='emerge -s' pinstall='sudo emerge -av' premove='sudo emerge -avC'
-  alias dupdate='sudo emerge --sync' dupgrade='sudo emerge -avuND @security' dcleanup='sudo emerge --ask --depclean'
+  alias dupdate='sudo emerge --sync' dupgrade='sudo emerge -avuND @world' dcleanup='sudo emerge --ask --depclean'
 fi
 
 # Mount
--- dot.files/.bashrc.d/functions.bash
+++ dot.files/.bashrc.d/functions.bash
@@ -33,6 +33,26 @@ countdown() {
 
 # UTILS =======================================================================
 
+pyhttpserv() {
+    #!/usr/bin/env bash
+    #
+    # pyhttpserv.bash Start an http server in current directory
+    # https://twitter.com/climagic/status/1224732676361461765
+    # python3 -m http.server 8080 # Start a simple webserver using python3 on external port 8080 and use the current directory you are in as the document root. Be careful with what you expose to the world. Use --bind 127.0.0.1 if you want to make it local only.
+    # Or the old days with python 2: python -m SimpleHTTPServer 8080
+
+    declare -a pv=( $(python --version) )
+
+    if [[ "${pv[1]}" =~ ^3.* ]]; then
+	python -m http.server 8080 --bind 127.0.0.1
+    elif [[ "${pv[1]}" =~ ^2.* ]]; then
+	python -m SimpleHTTPServer 8080
+    else
+	echo "Fatal: No suitable python version found!"
+	return 1
+    fi
+}
+
 # (R)un things in the background with (C)ustom niceness and cli switches in a (M)utex kind of way
 # Usage : rcm niceness executable command line arguments
 # Example: rcm 9 conky -qdc ~/.conkyrc
