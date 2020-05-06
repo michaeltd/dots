f5865a6999a1709ddf6df4f74166cc258f46e08c
michaeltd <tsouchlarakis@gmail.com>
2020-05-06 22:43:19 +0300

    grmbl

--- dot.files/sbin/update_bkps.bash
+++ dot.files/sbin/update_bkps.bash
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 #
 # ~/sbin/update_bkps.bash
+#
 # Configure bkp jobs with ~/.bkp.include.job files
+#
 # Prereq's you'll need for this to work:
+#
 # 0) add your users public key ($RCPNT) to root's keyring
 # 1) ~/.bkp.include.myawesomebkpjob
 # 2) ~/.bkp.exclude
 # fill out above files with include and exclude rules respectively
-# 3) Update eldir(where to bkp) utb(user to bkp) and recipient.
+# 3) Update bkpto(where to bkp) bkpfrom(user to read files from) and recipient(pubkey to encrypt to).
 # 4) Profit
-readonly ELDIR="/mnt/el/Documents/BKP/LINUX" UTB="paperjam" RCPNT="tsouchlarakis@gmail.com"
+readonly BKPTO="/mnt/el/Documents/BKP/LINUX" BKPFROM="paperjam" RCPNT="tsouchlarakis@gmail.com"
 
 echo -ne " -- $(basename "${BASH_SOURCE[0]}") --\n"
 
-declare -ra INC=( $($(type -P ls) /home/${UTB}/.bkp.include.*) )
+declare -ra INC=( $($(type -P ls) /home/${BKPFROM}/.bkp.include.*) )
 
-readonly EXC="/home/${UTB}/.bkp.exclude"
-
+readonly EXC="/home/${BKPFROM}/.bkp.exclude"
 
 # Full path executables, no aliases
 declare -ra \
@@ -33,19 +35,19 @@ for BKP in "${INC[@]}"; do
     declare -a ARCHEXT+=( "${BKP##*.}.tar.gz" )
 done
 
-if [[ -d "${ELDIR}" && "${EUID}" -eq "0" ]]; then
+if [[ -d "${BKPTO}" && "${EUID}" -eq "0" ]]; then
     for ((i = 0; i < ${#INC[*]}; i++ )); do
 	if [[ ${BKPS2DO[i]} == "enc" ]]; then
-	    ARCFL="${ELDIR}/${HOSTNAME}.${DT}.${TM}.${EP}.${ARCHEXT[i]}"
+	    ARCFL="${BKPTO}/${HOSTNAME}.${DT}.${TM}.${EP}.${ARCHEXT[i]}"
 	    #shellcheck disable=SC2086
 	    time "${NICEC[@]}" "${TARCM[@]}" "--file" "${ARCFL}" $(cat ${INC[i]})
 	else
-	    ENCFL="${ELDIR}/${HOSTNAME}.${DT}.${TM}.${EP}.${ARCHEXT[i]}.pgp"
+	    ENCFL="${BKPTO}/${HOSTNAME}.${DT}.${TM}.${EP}.${ARCHEXT[i]}.pgp"
 	    #shellcheck disable=SC2086
 	    time "${NICEC[@]}" "${TARCM[@]}" $(cat ${INC[i]}) | "${GPG2C[@]}" "${ENCFL}" "--encrypt"
 	fi
     done
 else
-    echo -ne "${ELDIR} not found or root access requirements not met.\n" >&2
+    echo -ne "${BKPTO} not found or root access requirements not met.\n" >&2
     exit 1
 fi
