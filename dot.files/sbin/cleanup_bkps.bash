a7fd114bce57c1d3548baca1cb0aa28919891cb1
michaeltd <tsouchlarakis@gmail.com>
2020-05-07 13:25:09 +0300

    I immediately regret this commit.

--- dot.files/sbin/cleanup_bkps.bash
+++ dot.files/sbin/cleanup_bkps.bash
@@ -5,77 +5,83 @@
 # that have an epoch date field in their filename seperated by periods(.) (eg: 190326.1553569476.enc.tar.gz.pgp)
 #
 #shellcheck source=/dev/null
-echo -ne " -- $(basename "${BASH_SOURCE[0]}") --\n"
 
-# BacKuPs Directory => BKPD,
-# BacKuPs to Keep => BKPK (in days),
-# BacKuPs Remove => BKPR (1 remove, 0 don't)
-BKPD="/mnt/el/Documents/BKP/LINUX" BKPK="14" BKPR="0" NOTHING2DO="0"
+main() {
+    
+    echo -ne " -- $(basename "${BASH_SOURCE[0]}") --\n"
 
-# Source explicitly for non interactive shells.
-SRCSPATH="$(dirname $(dirname $(realpath ${BASH_SOURCE[0]})))/.bashrc.d/.stdlib"
+    # BacKuPs Directory => BKPD,
+    # BacKuPs to Keep => BKPK (in days),
+    # BacKuPs Remove => BKPR (1 remove, 0 don't)
+    BKPD="/mnt/el/Documents/BKP/LINUX" BKPK="14" BKPR="0" NOTHING2DO="0"
 
-declare -ra srcs=( "${SRCSPATH}/time.bash" \
-                  "${SRCSPATH}/string.bash" \
-                  "${SRCSPATH}/math.bash" )
+    # Source explicitly for non interactive shells.
+    SRCSPATH="$(dirname $(dirname $(realpath ${BASH_SOURCE[0]})))/.bashrc.d/.stdlib"
 
-while [[ -n "${1}" ]]; do
-    case "${1}" in
-	"-b"|"--bkpdir") shift; BKPD="${1}";;
-	"-s"|"--simulate") BKPR="1";;
-	"-k"|"--keep") shift; BKPK="${1}";;
-	"-d"|"--debug") set -x;;
-	*) echo -ne "Usage: $(basename "${BASH_SOURCE[0]}") [-(-b)kpdir /backups/directory/] [-(-s)imulate] [-(-k)eep # (int, days. default: 14)] [-(-d)ebug (default: off)]\n" >&2; exit 1;;
-    esac
-    shift
-done
+    declare -ra srcs=( "${SRCSPATH}/time.bash" \
+			   "${SRCSPATH}/string.bash" \
+			   "${SRCSPATH}/math.bash" )
 
-# No root access or No backups directory
-# (( EUID != 0 )) && echo -ne "privileged access requirements not met.\n" >&2 && exit 1
-[[ ! -d "${BKPD}" ]] && echo -ne "${BKPD} is not a directory.\n" >&2 && exit 1
-
-if [[ -d "${SRCSPATH}" ]]; then
-    for src in "${srcs[@]}"; do
-	if [[ -r "${src}" ]]; then
-	    source "${src}"
-	else
-	    echo -ne "${src} not readable.\n" >&2
-	    exit 1
-	fi
+    while [[ -n "${1}" ]]; do
+	case "${1}" in
+	    "-b"|"--bkpdir") shift; BKPD="${1}";;
+	    "-s"|"--simulate") BKPR="1";;
+	    "-k"|"--keep") shift; BKPK="${1}";;
+	    "-d"|"--debug") set -x;;
+	    *) echo -ne "Usage: $(basename "${BASH_SOURCE[0]}") [-(-b)kpdir /backups/directory/] [-(-s)imulate] [-(-k)eep # (int, days. default: 14)] [-(-d)ebug (default: off)]\n" >&2; return 1;;
+	esac
+	shift
     done
-else
-    echo -ne "${SRCSPATH} not found.\n" >&2
-    exit 1
-fi
 
-#shellcheck disable=SC2207
-FILES=( $("$(type -P ls)" "-At1" "${BKPD}"/*.tar.gz* 2> /dev/null) )
+    # No root access or No backups directory
+    # (( EUID != 0 )) && echo -ne "privileged access requirements not met.\n" >&2 && return 1
+    [[ ! -d "${BKPD}" ]] && echo -ne "${BKPD} is not a directory.\n" >&2 && return 1
 
-for (( x = 0; x < ${#FILES[@]}; x++ )); do
-    BFN="$(basename "${FILES[x]}")"
-    for PART in $(split "${BFN}" .); do
-	if [[ "${PART}" =~ ^[0-9]{10}$ ]]; then
-	    FNS[x]="${BFN}"
-	    DTS[x]="${PART}"
-	fi
-    done
-done
-
-for (( y = 0; y < ${#FNS[@]}; y++ )); do
-    if [[ "$(epochdd "$(max "${DTS[@]}")" "${DTS[y]}")" -ge "${BKPK}" ]]; then
-	NOTHING2DO="1"
-	if [[ "${BKPR}" -eq "1" ]]; then
-	    if [[ "$(lastdayofmonth "@${DTS[y]}")" == "$(date +%d --date="@${DTS[y]}")" ]]; then
-		echo "Not running: ${bold}'mkdir -vp ${BKPD}/bkp && cp -v ${BKPD}/${FNS[y]} ${BKPD}/bkp/${FNS[y]}'${reset}"
+    if [[ -d "${SRCSPATH}" ]]; then
+	for src in "${srcs[@]}"; do
+	    if [[ -r "${src}" ]]; then
+		source "${src}"
+	    else
+		echo -ne "${src} not readable.\n" >&2
+		return 1
 	    fi
-	    echo "Not running: ${bold}'rm -v ${BKPD}/${FNS[y]}'${reset}"
-	else
-	    if [[ "$(lastdayofmonth "@${DTS[y]}")" == "$(date +%d --date="@${DTS[y]}")" ]]; then
-		mkdir -vp "${BKPD}/bkp" && cp -v "${BKPD}/${FNS[y]}" "${BKPD}/bkp/${FNS[y]}"
-	    fi
-	    rm -v "${BKPD}/${FNS[y]}"
-	fi
+	done
+    else
+	echo -ne "${SRCSPATH} not found.\n" >&2
+	return 1
     fi
- done
 
-[[ "${NOTHING2DO}" -eq "0" ]] && echo "Nothing left to do!" >&2
+    #shellcheck disable=SC2207
+    FILES=( $("$(type -P ls)" "-At1" "${BKPD}"/*.tar.gz* 2> /dev/null) )
+
+    for (( x = 0; x < ${#FILES[@]}; x++ )); do
+	BFN="$(basename "${FILES[x]}")"
+	for PART in $(split "${BFN}" .); do
+	    if [[ "${PART}" =~ ^[0-9]{10}$ ]]; then
+		FNS[x]="${BFN}"
+		DTS[x]="${PART}"
+	    fi
+	done
+    done
+
+    for (( y = 0; y < ${#FNS[@]}; y++ )); do
+	if [[ "$(epochdd "$(max "${DTS[@]}")" "${DTS[y]}")" -ge "${BKPK}" ]]; then
+	    NOTHING2DO="1"
+	    if [[ "${BKPR}" -eq "1" ]]; then
+		if [[ "$(lastdayofmonth "@${DTS[y]}")" == "$(date +%d --date="@${DTS[y]}")" ]]; then
+		    echo "Not running: ${bold}'mkdir -vp ${BKPD}/bkp && cp -v ${BKPD}/${FNS[y]} ${BKPD}/bkp/${FNS[y]}'${reset}"
+		fi
+		echo "Not running: ${bold}'rm -v ${BKPD}/${FNS[y]}'${reset}"
+	    else
+		if [[ "$(lastdayofmonth "@${DTS[y]}")" == "$(date +%d --date="@${DTS[y]}")" ]]; then
+		    mkdir -vp "${BKPD}/bkp" && cp -v "${BKPD}/${FNS[y]}" "${BKPD}/bkp/${FNS[y]}"
+		fi
+		rm -v "${BKPD}/${FNS[y]}"
+	    fi
+	fi
+    done
+
+    [[ "${NOTHING2DO}" -eq "0" ]] && echo "Nothing left to do!" >&2
+}
+
+[[ "${BASH_SOURCE[0]}" == "${0}" ]] && main "${@}"
--- dot.files/sbin/update_bkps.bash
+++ dot.files/sbin/update_bkps.bash
@@ -2,69 +2,89 @@
 #
 # ~/sbin/update_bkps.bash
 #
-# Configure bkp jobs with ~/.bkp.include.job files
-#
+# Configure bkp jobs with ~/.bkp.inc.(enc|cmp).job files
+# (enc.job files will be encrypted,
+# cmp.job files will be compressed)
 # Prereq's you'll need for this to work:
-#
-# 0) add your users public key ($RCPNT) to root's keyring
-# 1) ~/.bkp.include.myawesomebkpjob
-# 2) ~/.bkp.exclude
-# fill out above files with include and exclude rules respectively
+# 0) add your users public key ($RCPNT) to root's keyring.
+#    Root access is required for system wide backups.
+# 1) ~/.bkp.inc.(cmp|enc).job_desc
+# 2) ~/.bkp.exc (optional)
 # 3) Update bkpto(where to bkp) bkpfrom(user to read files from) and recipient(pubkey to encrypt to).
+#    Or call script with parameters: sudo update_bkps.bash -f username -t /my/bkps -k mykey@name.org
 # 4) Profit
-#
-# Example ~/.bkp.include.usr file:
-#
+# .bkp.inc.* file name explanation:
+# /home/paperjam/.bkp.inc.enc.job
+#        1        2    3   4   5 
+# 1) This part will be given by your [-(-f)rom] switch (default paperjam)
+#    The script will use it as a starting point to search for .bkp.inc.* files
+# 2 & 3) In .bkp.inc.* files we will store the job definitions
+# 4) This part should be aither *.enc.* or *.cmp.*.
+#    enc file definitions will get encrypted,
+#    cmp file definitions will get compressed.
+# 5) The last part is a job short description,
+#    It will end up in the resulting *.pgp or *.tar.gz files,
+#    so we know what we're dealing with at a quick glance.
+# Example ~/.bkp.inc.*.* file contents:
 # /home/paperjam/git/.
 # /home/paperjam/Documents/.
-#
-# Example ~/.bkp.exclude file:
-#
+# Example ~/.bkp.exc file contents:
 # */.git/*
 # */.github/*
 # */node_modules/*
-#
+# You can ommit exlude file safely
 
 main() {
     echo -ne " -- $(basename "${BASH_SOURCE[0]}") --\n"
+    # Some defaults
+    local BKPTO="/mnt/el/Documents/BKP/LINUX" BKPFROM="paperjam" RCPNT="tsouchlarakis@gmail.com"
 
-    local -r BKPTO="/mnt/el/Documents/BKP/LINUX" BKPFROM="paperjam" RCPNT="tsouchlarakis@gmail.com"
-    #shellcheck disable=SC2207
-    local -ra INC=( $($(type -P ls) /home/${BKPFROM}/.bkp.include.*) )
-
-    local -r EXC="/home/${BKPFROM}/.bkp.exclude"
-
-    # Full path executables, no aliases
-    local -ra \
-        NICEC=( "$(type -P nice)" "-n" "19" ) \
-        TARCM=( "$(type -P tar)" "--create" "--gzip" "--exclude-from=${EXC}" \
-	    "--exclude-backups" "--one-file-system" ) \
-        GPG2C=( "$(type -P gpg2)" "--batch" "--yes" "--quiet" "--recipient" \
-	    "${RCPNT}" "--trust-model" "always" "--output" )
-
-    local -r DT="$(date +%y%m%d)" TM="$(date +%H%M)" EP="$(date +%s)"
-
-    for BKP in "${INC[@]}"; do
-	local -a BKPS2DO+=( "${BKP##*.}" )
-	local -a ARCHEXT+=( "${BKP##*.}.tar.gz" )
+    while [[ -n "${1}" ]]; do
+	case "${1}" in
+	    "-t"|"--to") shift; BKPTO="${1}";;
+	    "-f"|"--from") shift; BKPFROM="${1}";;
+	    "-k"|"--key") shift; RCPNT="${1}";;
+	    "-d"|"--debug") set -x;;
+	    *) echo -ne "Usage: sudo $(basename "${BASH_SOURCE[0]}") [-(-t)o /backup/to/] [-(-f)rom username] [-(-k)ey some@email.org] [-(-d)ebug]\n" >&2; return 1;;
+	esac
+	shift
     done
 
-    if [[ -d "${BKPTO}" && "${EUID}" -eq "0" ]]; then
-	for ((i = 0; i < ${#INC[*]}; i++ )); do
-	    if [[ ${BKPS2DO[i]} == "enc" ]]; then
-		local ARCFL="${BKPTO}/${HOSTNAME}.${DT}.${TM}.${EP}.${ARCHEXT[i]}"
+    #shellcheck disable=SC2207
+    local -ra INC=( $($(type -P ls) /home/${BKPFROM}/.bkp.inc.*) )
+    [[ -r "/home/${BKPFROM}/.bkp.exc" ]] && local -r EXC="/home/${BKPFROM}/.bkp.exc"
+    local -r DT="$(date +%y%m%d)" TM="$(date +%H%M)" EP="$(date +%s)"
+    
+    # Full path executables, no aliases
+    local -ra \
+	  NICEC=( "$(type -P nice)" "-n" "19" ) \
+	  TARCM=( "$(type -P tar)" "--create" "--gzip" "$([[ -n "${EXC}" ]] && echo -n "--exclude-from=${EXC}")" "--exclude-backups" "--one-file-system" ) \
+	  GPG2C=( "$(type -P gpg2)" "--batch" "--yes" "--quiet" "--recipient" "${RCPNT}" "--trust-model" "always" "--output" )
+
+    # Some sanity checking ...
+    [[ ! -d "${BKPTO}" ]] && echo -ne "${BKPTO} not found.\n" >&2 && return 1
+    [[ ! -d "/home/${BKPFROM}" ]] && echo -ne "/home/${BKPFROM} not found.\n" >&2 && return 1
+    [[ -z "${INC[0]}" ]] && echo -ne "No job file definitions found.\nNothing left to do!" >&2 && return 1
+    [[ "${EUID}" -ne "0" ]] && echo -ne "Root access requirements not met.\n" >&2 && return 1
+
+    for ((i = 0; i < ${#INC[*]}; i++ )); do
+	[[ ${INC[i]} =~ (enc|cmp) ]] && local FUNC2DO="${BASH_REMATCH[1]}"
+	local ARCHEXT="${INC[i]##*.}.tar.gz"
+	case "${FUNC2DO}" in
+	    "cmp")
+		local CMPFL="${BKPTO}/${HOSTNAME}.${DT}.${TM}.${EP}.${ARCHEXT}"
 		#shellcheck disable=SC2086,SC2046
-		time "${NICEC[@]}" "${TARCM[@]}" "--file" "${ARCFL}" $(cat ${INC[i]})
-	    else
-		local ENCFL="${BKPTO}/${HOSTNAME}.${DT}.${TM}.${EP}.${ARCHEXT[i]}.pgp"
+		time "${NICEC[@]}" "${TARCM[@]}" "--file" "${CMPFL}" $(cat ${INC[i]})
+		;;
+	    "enc")
+		local ENCFL="${BKPTO}/${HOSTNAME}.${DT}.${TM}.${EP}.${ARCHEXT}.pgp"
 		#shellcheck disable=SC2086,SC2046
 		time "${NICEC[@]}" "${TARCM[@]}" $(cat ${INC[i]}) | "${GPG2C[@]}" "${ENCFL}" "--encrypt"
-	    fi
-	done
-    else
-	echo -ne "${BKPTO} not found or root access requirements not met.\n" >&2
-	return 1
-    fi
+		;;
+	    *)
+		echo -ne "Error in ${INC[i]} file name.\nIt needs a '*.enc|cmp.*' part in it.\n" >&2
+	esac
+    done
 }
 
 [[ "${BASH_SOURCE[0]}" == "${0}" ]] && main "${@}"
