aa5d1227f67819f1d9e26349a22e968b943a6aa1
michaeltd <tsouchlarakis@gmail.com>
2020-05-10 19:52:58 +0300

    I don't know why. Just move on.

--- dot.files/bin/michaeltd.bash
+++ dot.files/bin/michaeltd.bash
@@ -18,8 +18,7 @@
 # │      Card:  curl -sL https://tinyurl.com/mtd-card|sh  │
 # │                                                       │
 # ╰───────────────────────────────────────────────────────╯
-
-# Font attributes, Colors, bg colors
+# Font attributes, colors, bg colors
 declare -r reset="$(tput sgr0)" bold="$(tput bold)" dim="$(tput dim)" blink="$(tput blink)" underline="$(tput smul)" end_underline="$(tput rmul)" reverse="$(tput rev)" hidden="$(tput invis)" black="$(tput setaf 0)" red="$(tput setaf 1)" green="$(tput setaf 2)" yellow="$(tput setaf 3)" blue="$(tput setaf 4)" magenta="$(tput setaf 5)" cyan="$(tput setaf 6)" white="$(tput setaf 7)" default="$(tput setaf 9)" bg_black="$(tput setab 0)" bg_red="$(tput setab 1)" bg_green="$(tput setab 2)" bg_yellow="$(tput setab 3)" bg_blue="$(tput setab 4)" bg_magenta="$(tput setab 5)" bg_cyan="$(tput setab 6)" bg_white="$(tput setab 7)" bg_default="$(tput setab 9)"
 
 l[1]="    ${green}╭───────────────────────────────────────────────────────╮"
