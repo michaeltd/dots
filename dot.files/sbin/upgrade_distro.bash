2ec908b3ce773a632e72afc33ced2946be67bb7f
michaeltd <tsouchlarakis@gmail.com>
2020-05-19 03:44:30 +0300

    1589849070

--- dot.files/sbin/upgrade_distro.bash
+++ dot.files/sbin/upgrade_distro.bash
@@ -8,37 +8,43 @@
 set -u
 IFS=$'\t\n'
 
-echo -ne " -- ${BASH_SOURCE[0]##*/} --\n"
+main() {
 
-# For this to work package manager arrays must be in following format...
-# | #1 package manager executable | #2 repo update switch | #3 distro upgrade switch(es)| #4 ...
-# PS: By ignoring dpkg and rpm we are avoiding issues with systems where alien has been installed.
-#shellcheck disable=SC2034
-declare -ra apt_get=( "apt-get" "update" "--assume-yes" "--simulate" "dist-upgrade" ) \
-        yum=( "yum" "check-update" "update" ) \
-        zypper=( "zypper" "refresh" "update" "--no-confirm" "--auto-agree-with-licenses" ) \
-        pacman=( "pacman" "-Sy" "-Syu" ) \
-        emerge=( "emerge" "--sync" "--pretend" "--nospinner" "--update" "--deep" "--newuse" "${1:-@security}" ) \
-        pkg=( "pkg" "update" "upgrade" )
+    echo -ne " -- ${BASH_SOURCE[0]##*/} --\n"
 
-declare -ra pms=( apt_get[@] yum[@] zypper[@] pacman[@] emerge[@] pkg[@])
+    # For this to work package manager arrays must be in following format...
+    # | #1 package manager executable | #2 repo update switch | #3 distro upgrade switch(es)| #4 ...
+    # PS: By ignoring dpkg and rpm we are avoiding issues with systems where alien has been installed.
+    #shellcheck disable=SC2034
+    local -ra apt_get=( "apt-get" "update" "--assume-yes" "--simulate" "dist-upgrade" ) \
+            yum=( "yum" "check-update" "update" ) \
+            zypper=( "zypper" "refresh" "update" "--no-confirm" "--auto-agree-with-licenses" ) \
+            pacman=( "pacman" "-Sy" "-Syu" ) \
+            emerge=( "emerge" "--sync" "--pretend" "--nospinner" "--update" "--deep" "--newuse" "${1:-@security}" ) \
+            pkg=( "pkg" "update" "upgrade" )
 
-readonly notfound="404"
+    local -ra pms=( apt_get[@] yum[@] zypper[@] pacman[@] emerge[@] pkg[@])
 
-declare pmidx="${notfound}"
+    local -r notfound="404"
 
-# Which is the first available pm in this system?
-for x in "${!pms[@]}"; do
-    if [[ -n "$(type -P "${!pms[x]:0:1}")" ]]; then
-	pmidx="${x}"
-	break # break on first match.
+    local pmidx="${notfound}" x=0
+
+    # Which is the first available pm in this system?
+    for x in "${!pms[@]}"; do
+	if [[ -n "$(type -P "${!pms[x]:0:1}")" ]]; then
+	    local -r pmidx="${x}"
+	    break # break on first match.
+	fi
+    done
+
+    if (( pmidx == notfound || EUID != 0 )); then
+	#shellcheck disable=SC2154
+	printf " Error: required access privilages not met,\n or package manager not found. \n For this to work you need root account privilages \n and a %s, %s, %s, %s, %s or %s based distro.\n Quithing.\n" "${!pms[0]:0:1}" "${!pms[1]:0:1}" "${!pms[2]:0:1}" "${!pms[3]:0:1}" "${!pms[4]:0:1}" "${!pms[5]:0:1}" >&2
+	return 1
+    else
+	time "${!pms[pmidx]:0:1}" "${!pms[pmidx]:1:1}" && time "${!pms[pmidx]:0:1}" "${!pms[pmidx]:2}"
     fi
-done
 
-if (( pmidx == notfound || euid != 0 )); then
-    #shellcheck disable=SC2154
-    printf "Error: Package manager not found, or required access privilages not met.\n For this to work you need root account privilages and a \n %s, %s, %s, %s, %s or %s based distro.\n Quithing.\n" "${!pms[0]:0:1}" "${!pms[1]:0:1}" "${!pms[2]:0:1}" "${!pms[3]:0:1}" "${!pms[4]:0:1}" "${!pms[5]:0:1}" >&2
-    exit 1
-else
-    time "${!pms[pmidx]:0:1}" "${!pms[pmidx]:1:1}" && time "${!pms[pmidx]:0:1}" "${!pms[pmidx]:2}"
-fi
+}
+
+[[ "${BASH_SOURCE[0]}" == "${0}" ]] && main "${@}"
