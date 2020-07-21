cfdc29119c15cec36f8898f70d0621c83628e6bf
michaeltd <tsouchlarakis@gmail.com>
2020-07-21 05:43:55 +0300

    1595299435

--- dot.files/.bashrc.d/10_aliases.bash
+++ dot.files/.bashrc.d/10_aliases.bash
@@ -157,4 +157,4 @@ alias termbindotcom='nc termbin.com 9999'
 alias ytdla='youtube-dl --extract-audio --audio-format mp3 --prefer-ffmpeg --ignore-errors --no-check-certificate'
 alias ytdlv='youtube-dl --format mp4 --prefer-ffmpeg --ignore-errors --no-check-certificate'
 
-. ~/.other.src
+[[ -f ~/.other.src ]] && . ~/.other.src
