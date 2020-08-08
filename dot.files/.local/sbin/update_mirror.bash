f41813ea756d73b40fc0a525173fdf1684f78016
michaeltd <tsouchlarakis@gmail.com>
2020-08-08 14:52:21 +0300

    1596887541

--- dot.files/.local/sbin/update_mirror.bash
+++ dot.files/.local/sbin/update_mirror.bash
@@ -10,7 +10,8 @@ echo -ne " -- ${BASH_SOURCE[0]##*/} --\n"
 # Full path executables
 nicec=( "nice" "-n" "19" )
 
-rsncm=( "rsync" "--verbose" "--recursive" "--times" "--delete" "--exclude=*/Videos/*" )
+# rsncm=( "rsync" "--verbose" "--recursive" "--times" "--delete" "--exclude=*/Videos/*" )
+rsncm=( "rsync" "--recursive" "--times" "--delete" "--exclude=*/Videos/*" )
 
 elmnt="/mnt/el/Documents"
 
