0ace4a5d859381654abe16605db2d6de7e25291c
michaeltd <tsouchlarakis@gmail.com>
2020-09-25 02:10:54 +0300

    1600989054

--- dot.files/.local/bin/passcli.bash
+++ dot.files/.local/bin/passcli.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash --norc --noprofile
 #shellcheck shell=bash disable=SC1008,SC2096
 #
-# Simple password cli
+# Simple password cli. Consistent use of ${PAGER} ensures there's no password "bleed" from terminals with persistant history configured
 
 # Unofficial Bash Strict Mode
 set -euo pipefail
@@ -25,13 +25,15 @@ passcli() {
      ##.....::: #########::..... ##::..... ##: ##::::::: ##:::::::: ##::
      ##:::::::: ##.... ##:'##::: ##:'##::: ##: ##::: ##: ##:::::::: ##::
      ##:::::::: ##:::: ##:. ######::. ######::. ######:: ########:'####:
-     ..:::::::::..:::::..:::......::::......::::......:::........::....::
+    ..:::::::::..:::::..:::......::::......::::......:::........::....::
      "
 
     show_header() { clear; type -P lolcat &>/dev/null && echo "${pass_header}"|lolcat || echo "${pass_header}"; }
 
     usage() { echo -ne "\n Usage: ${sbn} add 'domain,mail,name,pass'|find keywd|rem keywd|show\n\n" >&2; }
 
+    purge_hist() { history -w; sed -i "/${sbn}/d" "${HOME}/.bash_history"; }
+    
     encrypt() { "${pgpc[@]}" "${pass_pgp}" "--encrypt" "${pass_file}" && "${shrc[@]}" {"${pass_file}","${pass_bck}"} 2> /dev/null; }
 
     #shellcheck disable=SC2015
@@ -52,7 +54,7 @@ passcli() {
     add() { echo "${*}" >> "${pass_file}"; show; }
 
     case "${*}" in
-	add*|rem*|find*|show*) show_header; decrypt; "${@}"; encrypt;;
+	add*|rem*|find*|show*) show_header; decrypt; "${@}"; encrypt; purge_hist;;
 	*) show_header; usage; return 1;;
     esac
 }
