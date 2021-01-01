a2b37346f3efadcfc26898bd22e5fd84faafe37d
michaeltd <tsouchlarakis@gmail.com>
2021-01-01 15:07:46 +0200

    1609506465

--- dot.files/.local/sbin/update_backups.bash
+++ dot.files/.local/sbin/update_backups.bash
@@ -1,36 +1,7 @@
 #!/usr/bin/env -S bash --norc --noprofile
 #shellcheck shell=bash disable=SC1008,SC2096,SC2155,SC2034,SC2046
 #
-# Configure backups with ~/.backup_include.(encrypt|compress).job_name definition files
-
-# Prereq's you'll need for this to work:
-# 1) ~/.backup_include.(compress|encrypt).job_name
-# 2) ~/.backup_exclude (optional, can be ommited safely)
-# 3) Update $definitions (location to read definitions from), $backup_to (where to backup) and $recipient (pubkey to encrypt to).
-#    Or call script with parameters: update_bkps.bash -f /path/to/defs -t /path/to/backups -k some@key.org
-# NOTE: add your $recipient public key to root's keyring, If you link this to /etc/cron.anything
-
-# .backup_include.*.* file name explanation:
-# /path/to/defs/.backup_include.compress|encrypt.job_name
-# '            '               '                '        '
-#       1              2                3           4
-# 1) This part will be given by your [-(-f)rom] switch (default "${HOME}")
-#    The script will use it as a starting point to search for all .backup_* related files.
-# 2) .backup_include.* will be the search term for the definitions array.
-# 3) This part should be aither *.encrypt.* or *.compress.*.
-#    encrypt file definitions will result in encrypted tarballs,
-#    compress file definitions will result in unencrypted tarballs.
-# 4) The fifth and last part serves as the jobs name.
-#    It will end up in the resulting *.pgp or *.tar.gz file name.
-
-# Sample ~/.backup_include.*.* file contents:
-# /home/username/git/.
-# /home/username/Documents/.
-
-# Sample ~/.backup_exclude file contents:
-# */.git/*
-# */.github/*
-# */node_modules/*
+# Configure backups with backup_include.(encrypt|compress).job_name definition files
 
 # Unofficial Bash Strict Mode
 set -uo pipefail
@@ -41,15 +12,45 @@ readonly sdn="$(dirname "$(realpath "${BASH_SOURCE[0]}")")" \
 	 sbn="$(basename "$(realpath "${BASH_SOURCE[0]}")")"
 
 main() {
-    local definitions="${HOME}" backup2="/mnt/data/Documents/bkp/linux" recipient="tsouchlarakis@gmail.com" niceness="19"
+    local definitions="${HOME}/.${sbn%%.*}" backup2="/mnt/data/Documents/bkp/linux" recipient="tsouchlarakis@gmail.com" niceness="19"
     local myusage="
     Usage: ${sbn} [-(-f)rom /path/to/defs] [-(-t)o /path/to/backups] [-(-k)ey some@key.org] [-(-n)iceness {0..19}] [-(-d)ebug]
 
-    -(-f)rom /path/to/defs            where to read definitions from.
-    -(-t)o /path/to/backups           where to save backups to.
-    -(-k)ey some@key.org 	      key to encrypt to.
-    -(-n)iceness {0..19} 	      niceness to run with.
-    -(-d)ebug		      	      display lots of words.
+    -(-f)rom /path/to/defs            where to read definitions from. Default: $definitions
+    -(-t)o /path/to/backups           where to save backups to. Default: $backup2
+    -(-k)ey some@key.org 	      key to encrypt to. Default: $recipient
+    -(-n)iceness {0..19} 	      niceness to run with. Default: $niceness
+    -(-d)ebug		      	      display lots of words. Default: OFF
+
+    Prereq's you'll need for this to work:
+    1) backup_include.(compress|encrypt).job_name
+    2) backup_exclude (optional, can be ommited safely)
+    3) Update $definitions (location to read definitions from), \$backup_to (where to backup) and \$recipient (pubkey to encrypt to).
+       Or call script with parameters: update_bkps.bash -f /path/to/defs -t /path/to/backups -k some@key.org
+       NOTE: add your \$recipient public key to root's keyring, If you link this to /etc/cron.anything
+
+    backup_include.*.* file name explanation:
+    /path/to/defs/backup_include.compress|encrypt.job_name
+    '            '              '                '        '
+          1              2                3           4
+    1) This part will be given by your [-(-f)rom] switch (default \${HOME}/.\${sbn%%.*})
+       The script will use it as a starting point to search for all backup_* related files.
+    2) backup_include.* will be the search term for the definitions array.
+    3) This part should be aither *.encrypt.* or *.compress.*.
+       encrypt file definitions will result in encrypted tarballs,
+       compress file definitions will result in unencrypted tarballs.
+    4) The fifth and last part serves as the jobs name.
+       It will end up in the resulting *tar.gz.pgp or *.tar.gz file name.
+
+    Sample backup_include.*.* file contents:
+    /home/username/git/.
+    /home/username/Documents/.
+
+    Sample backup_exclude file contents:
+    */.git/*
+    */.github/*
+    */node_modules/*
+
 "
 
     log2err() { echo -ne "${sbn}: ${*}\n" >&2; }
@@ -66,8 +67,8 @@ main() {
 	shift
     done
 
-    local -ra includes=( "${definitions}"/.backup_include.* )
-    local -r exclude="${definitions}/.backup_exclude" job_fn="${backup2}/${HOSTNAME}.$(date -u +%y%m%d.%H%M.%s)"
+    local -ra includes=( "${definitions}"/backup_include.* )
+    local -r exclude="${definitions}/backup_exclude" job_fn="${backup2}/${HOSTNAME}.$(date -u +%y%m%d.%H%M.%s)"
 
     [[ -r "${includes[0]}" ]] || { log2err "No readable job file definitions found.\nNothing left to do!"; return 1; }
     [[ -d "${backup2}" ]] || { log2err "${backup2} is not a directory."; return 1; }
@@ -78,14 +79,26 @@ main() {
 
     compress() {
 	local job_out="${job_fn}.${1##*.}.tar.gz"
-	log2err "Writing: ${job_out##*/}"
 	"${nice_cmd[@]}" "${tar_cmd[@]}" "--file" "${job_out}" $(cat "${1}")
+	local err=$?
+	if (( err == 0 )); then
+	    log2err "Wrote: ${job_out##*/}"
+	else
+	    rm -f "${job_out}" # Wipe half baked archives (from reboots, shutdowns, etc)
+	    log2err "Discarded: ${job_out##*/}, error level is $err. Job Failed!"
+	fi
     }
 
     encrypt() {
 	local job_out="${job_fn}.${1##*.}.tar.gz.pgp"
-	log2err "Writing: ${job_out##*/}"
 	"${nice_cmd[@]}" "${tar_cmd[@]}" $(cat "${1}") | "${pgp_cmd[@]}" "${job_out}" "--encrypt"
+	local err=$?
+	if (( err == 0 )); then
+	    log2err "Wrote: ${job_out##*/}"
+	else
+	    rm -f "${job_out}" # Wipe half baked archives (from reboots, shutdowns, etc)
+	    log2err "Discarded: ${job_out##*/}, error level is $err. Job Failed!"
+	fi
     }
 
     for include in "${includes[@]}"; do
