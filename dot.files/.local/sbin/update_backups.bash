85d14f2299df881983f544cb7a66e41ebb529fdf
michaeltd <tsouchlarakis@gmail.com>
2021-01-05 10:11:58 +0200

    1609834317

--- dot.files/.local/sbin/update_backups.bash
+++ dot.files/.local/sbin/update_backups.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash --norc --noprofile
 #shellcheck shell=bash disable=SC1008,SC2096,SC2155,SC2034,SC2046
 #
-# Configure backups with backup_include.(encrypt|compress).job_name definition files
+# Configure backups with ${sbn%%.*}.include.(encrypt|compress).job_name definition files
 
 # Unofficial Bash Strict Mode
 set -uo pipefail
@@ -14,39 +14,41 @@ readonly sdn="$(dirname "$(realpath "${BASH_SOURCE[0]}")")" \
 main() {
     local definitions="${HOME}/.${sbn%%.*}" backup2="/mnt/data/Documents/bkp/linux" recipient="tsouchlarakis@gmail.com" niceness="19"
     local myusage="
-    Usage: ${sbn} [-(-f)rom /path/to/defs] [-(-t)o /path/to/backups] [-(-k)ey some@key.org] [-(-n)iceness {0..19}] [-(-d)ebug]
+    Usage: ${sbn} [-(-f)rom /path/to/defs] [-(-t)o /path/to/backups] [-(-k)ey some@key.org] [-(-n)iceness {0..19}] [-(-d)ebug] [-(-h)elp]
 
     -(-f)rom /path/to/defs            where to read definitions from. Default: $definitions
     -(-t)o /path/to/backups           where to save backups to. Default: $backup2
     -(-k)ey some@key.org 	      key to encrypt to. Default: $recipient
     -(-n)iceness {0..19} 	      niceness to run with. Default: $niceness
     -(-d)ebug		      	      display lots of words. Default: OFF
+    -(-h)elp			      This screen.
 
     Prereq's you'll need for this to work:
-    1) backup_include.(compress|encrypt).job_name
-    2) backup_exclude (optional, can be ommited safely)
-    3) Update $definitions (location to read definitions from), \$backup_to (where to backup) and \$recipient (pubkey to encrypt to).
-       Or call script with parameters: update_bkps.bash -f /path/to/defs -t /path/to/backups -k some@key.org
+    1) Some include/exclude files to describe your backup jobs, -f switch. (see bellow)
+    2) A path to place your backup files, -t switch.
+    3) A \$recipient public key to encrypt to, -k switch.
        NOTE: add your \$recipient public key to root's keyring, If you link this to /etc/cron.anything
 
-    backup_include.*.* file name explanation:
-    /path/to/defs/backup_include.compress|encrypt.job_name
-    '            '              '                '        '
-          1              2                3           4
-    1) This part will be given by your [-(-f)rom] switch (default \${HOME}/.\${sbn%%.*})
-       The script will use it as a starting point to search for all backup_* related files.
-    2) backup_include.* will be the search term for the definitions array.
+    Include/Exclude file details.
+
+    ${sbn%%.*}.include.*.* file name explanation:
+    /path/to/defs/${sbn%%.*}.include.compress|encrypt.job_name
+    '            '                      '                '        '
+          1                 2                    3            4
+    1) This part will be given by your [-(-f)rom] switch (default \"${definitions}\")
+       The script will use it as a starting point to search for all ${sbn%%.*}.* related files.
+    2) ${sbn%%.*}.include.* will be the search term for the definitions files.
     3) This part should be aither *.encrypt.* or *.compress.*.
        encrypt file definitions will result in encrypted tarballs,
        compress file definitions will result in unencrypted tarballs.
-    4) The fifth and last part serves as the jobs name.
-       It will end up in the resulting *tar.gz.pgp or *.tar.gz file name.
+    4) The last part serves as the jobs name.
+       It will end up in the resulting *.tar.gz.pgp or *.tar.gz file name.
 
-    Sample backup_include.*.* file contents:
+    Sample ${sbn%%.*}.include.*.* file contents:
     /home/username/git/.
     /home/username/Documents/.
 
-    Sample backup_exclude file contents:
+    Sample ${sbn%%.*}.exclude file contents:
     */.git/*
     */.github/*
     */node_modules/*
@@ -62,13 +64,14 @@ main() {
 	    -k|--key) shift; recipient="${1}";;
 	    -n|--niceness) shift; [[ "${1}" =~ ^[-|+]?[0-9]+?$ ]] && (( $1 >= 0 && $1 <= 19 )) && niceness="${1}";;
 	    -d|--debug) set -x;;
-	    *) log2err "${myusage}"; return 1;;
+	    -h|--help) log2err "${myusage}"; return 1;;
+	    *) log2err "Unknown option ${1}\n${myusage}"; return 1;;
 	esac
 	shift
     done
 
-    local -ra includes=( "${definitions}"/backup_include.* )
-    local -r exclude="${definitions}/backup_exclude" job_fn="${backup2}/${HOSTNAME}.$(date -u +%y%m%d.%H%M.%s)"
+    local -ra includes=( "${definitions}/${sbn%%.*}.include".* )
+    local -r exclude="${definitions}/${sbn%%.*}.exclude" job_fn="${backup2}/${HOSTNAME}.$(date -u +%y%m%d.%H%M.%s)"
 
     [[ -r "${includes[0]}" ]] || { log2err "No readable job file definitions found.\nNothing left to do!"; return 1; }
     [[ -d "${backup2}" ]] || { log2err "${backup2} is not a directory."; return 1; }
@@ -85,7 +88,7 @@ main() {
 	    log2err "Wrote: ${job_out##*/}"
 	else
 	    rm -f "${job_out}" # Wipe half baked archives (from reboots, shutdowns, etc)
-	    log2err "Discarded: ${job_out##*/}, error level is $err. Job Failed!"
+	    log2err "Discarded: ${job_out##*/}, error level is $err. Job \"${1##*.}\" Failed!"
 	fi
     }
 
@@ -97,7 +100,7 @@ main() {
 	    log2err "Wrote: ${job_out##*/}"
 	else
 	    rm -f "${job_out}" # Wipe half baked archives (from reboots, shutdowns, etc)
-	    log2err "Discarded: ${job_out##*/}, error level is $err. Job Failed!"
+	    log2err "Discarded: ${job_out##*/}, error level is $err. Job \"${1##*.}\" Failed!"
 	fi
     }
 
